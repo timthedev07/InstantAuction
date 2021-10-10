b51b3fd5cc6fca276a3e73c93ea1200487f843ab
Tim <harvard90873@gmail.com>
2021-10-10 15:48:59 +0200

    shared: add backend url

--- packages/shared/src/constants/servers.ts
+++ packages/shared/src/constants/servers.ts
@@ -1,5 +1,9 @@
 export const FRONTEND =
   process.env.NODE_ENV === "production"
-    ? "https://yourdoge.vercel.app"
+    ? "https://instantauction.vercel.app"
     : "http://localhost:3000";
 export const PLAYGROUND = "https://studio.apollographql.com";
+export const BACKEND =
+  process.env.NODE_ENV === "production"
+    ? "https://instantauction.herokuapp.com"
+    : "http://localhost:4000";

84cd6f4ec1d3e9915fcd1578667f23338aa02504
Tim <harvard90873@gmail.com>
2021-10-10 10:36:39 +0200

    init web directory with nextjs

---
+++ packages/web/next.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const paths = [{ name: "shared", path: path.resolve(__dirname, "../shared") }];
+
+/** @type {import('next').NextConfig} */
+module.exports = () => {
+  return {
+    webpack(config, { defaultLoaders }) {
+      config.module.rules.push({
+        test: /\.(ts|tsx)$/,
+        include: [path.resolve(__dirname, "../shared")],
+        use: [defaultLoaders.babel],
+      });
+
+      paths.forEach((module) => {
+        config.resolve.alias[module.name] = module.path;
+      });
+      return config;
+    },
+  };
+};

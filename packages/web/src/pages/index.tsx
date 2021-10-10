c748e2c2991e2e4bc6f6abcf4fe0fa65589dae9a
Tim <harvard90873@gmail.com>
2021-10-10 17:26:23 +0200

    try fetch data

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -1,7 +1,9 @@
 import { NextPage } from "next";
+import { useHelloQuery } from "../../../shared/src";
 
 const Landing: NextPage = () => {
-  return <>Landing Page</>;
+  const { data, loading } = useHelloQuery();
+  return <h1>{loading ? "Loading..." : data}</h1>;
 };
 
 export default Landing;

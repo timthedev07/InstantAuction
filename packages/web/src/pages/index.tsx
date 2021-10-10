807b0916713c239a8323c58c66cf6d17f2e4ef22
Tim <harvard90873@gmail.com>
2021-10-10 17:39:11 +0200

    web: try if query succeeds

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -1,9 +1,9 @@
 import { NextPage } from "next";
-import { useHelloQuery } from "../../../shared/src";
+import { useHelloQuery } from "shared";
 
 const Landing: NextPage = () => {
   const { data, loading } = useHelloQuery();
-  return <h1>{loading ? "Loading..." : data}</h1>;
+  return <h1>{loading ? "Loading..." : JSON.stringify(data)}</h1>;
 };
 
 export default Landing;

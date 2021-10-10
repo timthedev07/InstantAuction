3189423695fc86bb42c01931ffcd32e0553cd56c
Tim <harvard90873@gmail.com>
2021-10-10 16:02:36 +0200

    web: wrap app in apollo provider

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -1,12 +1,21 @@
 // import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "../components/layout";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { BACKEND } from "shared";
+
+const client = new ApolloClient({
+  uri: BACKEND,
+  cache: new InMemoryCache(),
+});
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ApolloProvider client={client}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ApolloProvider>
   );
 };
 

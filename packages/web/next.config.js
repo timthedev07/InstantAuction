3422ba115ba5adb342475e6a2ed72221e596f1f4
Tim <harvard90873@gmail.com>
2021-10-11 12:57:07 +0200

    web: update client controllers path

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,5 +1,11 @@
 const path = require("path");
-const paths = [{ name: "shared", path: path.resolve(__dirname, "../shared") }];
+const clientControllersDirName = "client-controllers";
+const paths = [
+  {
+    name: clientControllersDirName,
+    path: path.resolve(__dirname, `../${clientControllersDirName}`),
+  },
+];
 
 /** @type {import('next').NextConfig} */
 module.exports = () => {
@@ -7,7 +13,7 @@ module.exports = () => {
     webpack(config, { defaultLoaders }) {
       config.module.rules.push({
         test: /\.(ts|tsx)$/,
-        include: [path.resolve(__dirname, "../shared")],
+        include: [path.resolve(__dirname, `../${clientControllersDirName}`)],
         use: [defaultLoaders.babel],
       });
 

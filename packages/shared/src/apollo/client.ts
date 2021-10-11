bbc86d9b09aaea8f9e6213e194d94d7ecf95bc77
Tim <harvard90873@gmail.com>
2021-10-11 12:23:24 +0200

    shared: delete frontend specific code

--- packages/shared/src/apollo/client.ts
+++
@@ -1,7 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { BACKEND } from "../constants/servers";
-
-export const client = new ApolloClient({
-  uri: `${BACKEND}/graphql`,
-  cache: new InMemoryCache(),
-});

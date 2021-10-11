8510c7a195c47fa8db49cfe2161289463bbeb3ff
Tim <harvard90873@gmail.com>
2021-10-11 13:24:33 +0200

    web: correct import

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -1,9 +1,13 @@
 import { NextPage } from "next";
-import { useHelloQuery } from "shared";
+import { useHelloQuery } from "client-controllers";
 
 const Landing: NextPage = () => {
   const { data, loading } = useHelloQuery();
-  return <h1>{loading ? "Loading..." : JSON.stringify(data)}</h1>;
+  return (
+    <h1 className="text-3xl">
+      {loading ? "Loading..." : data?.hello || "No data returned"}
+    </h1>
+  );
 };
 
 export default Landing;

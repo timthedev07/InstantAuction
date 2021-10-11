f392736d2edd542ee022adddbbbe98f7f0beea49
Tim <harvard90873@gmail.com>
2021-10-11 11:59:11 +0200

    shared: export this one too

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,3 +1,4 @@
-export * from "./constants"
-export * from "./utils"
-export * from "./generated/graphql"
+export * from "./constants";
+export * from "./utils";
+export * from "./generated/graphql";
+export * from "./apollo";

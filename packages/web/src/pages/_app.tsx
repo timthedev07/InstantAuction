a0cf38f19e0b1fb9cc9f814d1235cd73921a7c43
Tim <harvard90873@gmail.com>
2021-10-11 12:49:11 +0200

    web: import css

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -1,4 +1,4 @@
-// import "../styles/globals.css";
+import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "../components/layout";
 import { ApolloProvider } from "@apollo/client";

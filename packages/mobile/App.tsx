4562cffb7ccda144bb9ca514fdae2badf106523d
Tim <harvard90873@gmail.com>
2021-10-11 16:23:59 +0200

    mobile: wrap app inside of apollo provider

--- packages/mobile/App.tsx
+++ packages/mobile/App.tsx
@@ -2,14 +2,18 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import {useHelloQuery} from "client-controllers"
+import { ApolloProvider } from "@apollo/client";
+import { client } from "client-controllers";
 
 export default function App() {
   const {data, loading} = useHelloQuery();
   return (
-    <View style={styles.container}>
-      <Text>{loading ? "Loading" : data?.hello || "No data form the API"}</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ApolloProvider client={client}>
+      <View style={styles.container}>
+        <Text>{loading ? "Loading" : data?.hello || "No data form the API"}</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ApolloProvider>
   );
 }
 

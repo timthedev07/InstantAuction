4d9999064a8e1eb004e78ed1991a2843454db7b1
Tim <harvard90873@gmail.com>
2021-10-13 18:05:38 +0200

    server: use redis as session store

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -8,11 +8,22 @@ import { buildSchema } from "type-graphql";
 import cookieParser from "cookie-parser";
 import { createConnection } from "typeorm";
 import { FRONTEND, PLAYGROUND } from "shared";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { redisClient } from "./redis";
+import { __prod__ } from "./constants/prod";
 
 const PORT = parseInt(process.env.PORT || "4000");
 const HOSTNAME = process.env.HOST || "0.0.0.0";
 
 (async () => {
+  // check for environment variables before anything
+  const sessionSecret = process.env.SESSION_SECRET;
+  if (!sessionSecret || sessionSecret.length < 1) {
+    console.error("Session secret not specified");
+    process.exit(1);
+  }
+
   const app = express();
   app.use(cookieParser());
   app.use(express.json());
@@ -20,6 +31,23 @@ const HOSTNAME = process.env.HOST || "0.0.0.0";
     cors({
       credentials: true,
       origin: [FRONTEND, PLAYGROUND],
+    })
+  );
+  const RedisStore = connectRedis(session);
+  app.use(
+    session({
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+        httpOnly: true,
+        secure: __prod__, // cookie only works in https
+        sameSite: "lax",
+      },
+      secret: sessionSecret,
+      resave: false,
     })
   );
 

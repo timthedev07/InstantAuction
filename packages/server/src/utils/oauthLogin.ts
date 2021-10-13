51726066c099a3a7da1e14ad2b93485f24886cc0
Tim <harvard90873@gmail.com>
2021-10-13 18:24:20 +0200

    server: extend session data to allow custom props

--- packages/server/src/utils/oauthLogin.ts
+++ packages/server/src/utils/oauthLogin.ts
@@ -2,13 +2,21 @@ import { User } from "../entity/User";
 import { Request, Response } from "express";
 import { OAuthResponse } from "../resolvers/UserResolvers";
 
+declare module "express-session" {
+  interface SessionData {
+    userId: number;
+  }
+}
+
 export const loginOAuth = (
   user: User,
   request: Request,
   response: Response
 ) => {
   // TODO: Implement login logic with session
-  request.session.cookie;
+  const session = request.session;
+
+  session.userId;
 
   return {
     user,

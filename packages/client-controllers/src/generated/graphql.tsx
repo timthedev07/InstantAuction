9526d81812547fcdf5f1d1efd6c0a09c86c3bb88
Tim <harvard90873@gmail.com>
2021-10-13 19:55:06 +0200

    client-controllers: generate new graphql hook

--- packages/client-controllers/src/generated/graphql.tsx
+++ packages/client-controllers/src/generated/graphql.tsx
@@ -14,15 +14,55 @@ export type Scalars = {
   Float: number;
 };
 
+export type Mutation = {
+  __typename?: 'Mutation';
+  googleOAuth: OAuthResponse;
+};
+
+
+export type MutationGoogleOAuthArgs = {
+  email: Scalars['String'];
+  family_name: Scalars['String'];
+  given_name: Scalars['String'];
+  hd?: Maybe<Scalars['String']>;
+  id: Scalars['String'];
+  locale: Scalars['String'];
+  name: Scalars['String'];
+  picture?: Maybe<Scalars['String']>;
+  verified_email: Scalars['Boolean'];
+};
+
+export type OAuthResponse = {
+  __typename?: 'OAuthResponse';
+  user?: Maybe<User>;
+};
+
 export type Query = {
   __typename?: 'Query';
   hello: Scalars['String'];
+  me: User;
+};
+
+export type User = {
+  __typename?: 'User';
+  avatarUrl: Scalars['String'];
+  email: Scalars['String'];
+  id: Scalars['Int'];
+  provider?: Maybe<Scalars['String']>;
+  reputation: Scalars['Int'];
+  transactionCount: Scalars['Int'];
+  username: Scalars['String'];
 };
 
 export type HelloQueryVariables = Exact<{ [key: string]: never; }>;
 
 
 export type HelloQuery = { __typename?: 'Query', hello: string };
+
+export type MeQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type MeQuery = { __typename?: 'Query', me: { __typename?: 'User', id: number, email: string, username: string, provider?: string | null | undefined, avatarUrl: string, transactionCount: number, reputation: number } };
 
 
 export const HelloDocument = gql`
@@ -57,3 +97,43 @@ export function useHelloLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<Hell
 export type HelloQueryHookResult = ReturnType<typeof useHelloQuery>;
 export type HelloLazyQueryHookResult = ReturnType<typeof useHelloLazyQuery>;
 export type HelloQueryResult = Apollo.QueryResult<HelloQuery, HelloQueryVariables>;
+export const MeDocument = gql`
+    query Me {
+  me {
+    id
+    email
+    username
+    provider
+    avatarUrl
+    transactionCount
+    reputation
+  }
+}
+    `;
+
+/**
+ * __useMeQuery__
+ *
+ * To run a query within a React component, call `useMeQuery` and pass it any options that fit your needs.
+ * When your component renders, `useMeQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useMeQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useMeQuery(baseOptions?: Apollo.QueryHookOptions<MeQuery, MeQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<MeQuery, MeQueryVariables>(MeDocument, options);
+      }
+export function useMeLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<MeQuery, MeQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<MeQuery, MeQueryVariables>(MeDocument, options);
+        }
+export type MeQueryHookResult = ReturnType<typeof useMeQuery>;
+export type MeLazyQueryHookResult = ReturnType<typeof useMeLazyQuery>;
+export type MeQueryResult = Apollo.QueryResult<MeQuery, MeQueryVariables>;

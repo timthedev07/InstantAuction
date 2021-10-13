29667277242162ffbb2c962da02cc110a6c66e3a
Tim <harvard90873@gmail.com>
2021-10-13 18:11:03 +0200

    server: take more params

--- packages/server/src/utils/oauthLogin.ts
+++ packages/server/src/utils/oauthLogin.ts
@@ -1,9 +1,15 @@
 import { User } from "../entity/User";
-import { Response } from "express";
+import { Request, Response } from "express";
 import { OAuthResponse } from "../resolvers/UserResolvers";
 
-export const loginOAuth = (user: User, response: Response) => {
+export const loginOAuth = (
+  user: User,
+  request: Request,
+  response: Response
+) => {
   // TODO: Implement login logic with session
+  request.session.cookie;
+
   return {
     user,
   } as OAuthResponse;

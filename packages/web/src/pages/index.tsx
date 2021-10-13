59560745e65f759506568561725d34eed7752b16
Tim <harvard90873@gmail.com>
2021-10-13 21:06:52 +0200

    web: test - dry to get user data

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -1,12 +1,22 @@
 import { NextPage } from "next";
-import { useHelloQuery } from "client-controllers";
+import { useHelloQuery, useMeQuery } from "client-controllers";
 
 const Landing: NextPage = () => {
-  const { data, loading } = useHelloQuery();
+  const { data: testData, loading: testLoading } = useHelloQuery();
+  const { data, loading, error } = useMeQuery();
   return (
-    <h1 className="text-3xl">
-      {loading ? "Loading..." : data?.hello || "No data returned"}
-    </h1>
+    <>
+      <h1 className="text-3xl">
+        {testLoading ? "Loading..." : testData?.hello || "No data returned"}
+      </h1>
+      <pre>
+        {loading
+          ? "loading..."
+          : error
+          ? JSON.stringify(error, null, 2)
+          : JSON.stringify(data, null, 2)}
+      </pre>
+    </>
   );
 };
 

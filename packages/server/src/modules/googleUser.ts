71b81cbb3c19c5e206f70a041241b0d97d6a6e83
Tim <harvard90873@gmail.com>
2021-10-15 20:31:41 +0200

    server: remove graphql stuff from GoogleUser type

--- packages/server/src/modules/googleUser.ts
+++ packages/server/src/modules/googleUser.ts
@@ -1,31 +1,11 @@
-import { Field, ArgsType } from "type-graphql";
-
-@ArgsType()
-export class GoogleUser {
-  @Field()
+export interface GoogleUser {
   id: string;
-
-  @Field()
   email: string;
-
-  @Field()
   verified_email: boolean;
-
-  @Field()
   name: string;
-
-  @Field()
   given_name: string;
-
-  @Field()
   family_name: string;
-
-  @Field(() => String, { nullable: true })
   picture: string | null;
-
-  @Field()
   locale: string;
-
-  @Field(() => String, { nullable: true })
   hd: string | null;
 }

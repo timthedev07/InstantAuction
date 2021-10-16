882b77983efdfec4eaad1442bdfd6c1e11619a6c
Tim <harvard90873@gmail.com>
2021-10-16 23:18:02 +0200

    client-controllers: simplify type annotations

--- packages/client-controllers/src/hook-options/logoutOptions.ts
+++ packages/client-controllers/src/hook-options/logoutOptions.ts
@@ -1,19 +1,7 @@
-import {
-  MutationFunctionOptions,
-  DefaultContext,
-  ApolloCache,
-} from "@apollo/client";
-import { LogoutMutation, Exact, MeQuery, MeDocument } from "../generated/graphql";
+import { MeQuery, MeDocument, LogoutMutationOptions } from "../generated/graphql";
 
 export const logoutOptions:
-  | MutationFunctionOptions<
-      LogoutMutation,
-      Exact<{
-        [key: string]: never;
-      }>,
-      DefaultContext,
-      ApolloCache<any>
-    >
+  | LogoutMutationOptions
   | undefined = {
   update: (store, { data }) => {
     if (!data?.logout) return;

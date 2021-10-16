01423da6a3ad6a1abc74928431cb4d2fe43866d1
Tim <harvard90873@gmail.com>
2021-10-16 18:51:07 +0200

    server: create logout resolver

---
+++ packages/server/src/resolvers/users/Logout.ts
@@ -0,0 +1,22 @@
+import { Ctx, Query, Resolver } from "type-graphql";
+import { sessionCookieName } from "../../constants/session";
+import { NetworkingContext } from "../../types/NetworkingContext";
+
+@Resolver()
+export class LogoutResolver {
+  @Query(() => Boolean)
+  async logout(@Ctx() { req, res }: NetworkingContext) {
+    return new Promise<boolean>((resolve) =>
+      req.session.destroy((err) => {
+        res.clearCookie(sessionCookieName);
+        if (err) {
+          console.error(err);
+          resolve(false);
+          return;
+        }
+
+        resolve(true);
+      })
+    );
+  }
+}

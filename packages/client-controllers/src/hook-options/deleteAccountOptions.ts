43894556e413b54062cf185c800502d923510384
Tim <harvard90873@gmail.com>
2021-10-16 23:28:51 +0200

    client-controllers: modify delete account options
    turn variable into a function since it takes on variables

--- packages/client-controllers/src/hook-options/deleteAccountOptions.ts
+++ packages/client-controllers/src/hook-options/deleteAccountOptions.ts
@@ -1,16 +1,19 @@
-import { MeQuery, MeDocument, DeleteAccountMutationOptions } from "../generated/graphql";
+import { MeQuery, MeDocument, DeleteAccountMutationOptions, DeleteAccountMutationVariables } from "../generated/graphql";
 
-export const deleteAccountOptions:
-  | DeleteAccountMutationOptions
-  | undefined = {
-  update: (store, { data }) => {
-    if (data?.deleteAccount === undefined) return;
-    store.writeQuery<MeQuery>({
-      query: MeDocument,
-      data: {
-        __typename: "Query",
-        me: null,
-      },
-    });
-  },
+export const createDeleteAccountOptions = (
+  variables:DeleteAccountMutationVariables
+): DeleteAccountMutationOptions => {
+  return {
+    variables,
+    update: (store, { data }) => {
+      if (data?.deleteAccount === undefined) return;
+      store.writeQuery<MeQuery>({
+        query: MeDocument,
+        data: {
+          __typename: "Query",
+          me: null,
+        },
+      });
+    },
+  };
 };

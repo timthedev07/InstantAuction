99e604c8fa6b3263624139f1d4f8ef4248ce3d87
Tim <harvard90873@gmail.com>
2021-10-16 21:26:49 +0200

    server: include delete account resolver

--- packages/server/src/schema.ts
+++ packages/server/src/schema.ts
@@ -3,10 +3,17 @@ import { HelloResolver } from "./resolvers/Hello";
 import { MeResolver } from "./resolvers/users/Me";
 import { LogoutResolver } from "./resolvers/users/Logout";
 import { OAuthResolver } from "./resolvers/users/OAuth";
+import { DeleteAccountResolver } from "./resolvers/users/DeleteAccount";
 
 export const createSchema = () =>
   buildSchema({
-    resolvers: [HelloResolver, MeResolver, OAuthResolver, LogoutResolver],
+    resolvers: [
+      HelloResolver,
+      MeResolver,
+      OAuthResolver,
+      LogoutResolver,
+      DeleteAccountResolver,
+    ],
     authChecker: ({ context: { req } }) => {
       return !!req.session.userId;
     },

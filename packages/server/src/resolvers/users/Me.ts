5e7f22791ceb255709179a570e10bc14a0e648a4
Tim <harvard90873@gmail.com>
2021-10-16 19:16:59 +0200

    server: return null instead on failure

--- packages/server/src/resolvers/users/Me.ts
+++ packages/server/src/resolvers/users/Me.ts
@@ -1,25 +1,24 @@
 import { Ctx, Query, Resolver } from "type-graphql";
-import { notAuthenticatedErrorMessage } from "../../constants/errorMessages";
 import { User } from "../../entity/User";
 import { NetworkingContext } from "../../types/NetworkingContext";
 @Resolver()
 export class MeResolver {
-  @Query(() => User)
+  @Query(() => User, { nullable: true })
   async me(@Ctx() { req }: NetworkingContext) {
     const userId = req.session.userId;
 
     if (!userId) {
-      throw new Error(notAuthenticatedErrorMessage);
+      return null;
     }
 
     try {
       const user = await User.findOne({ where: { id: userId } });
       if (!user) {
-        throw new Error(notAuthenticatedErrorMessage);
+        return null;
       }
       return user;
     } catch (err) {
-      throw new Error(notAuthenticatedErrorMessage);
+      return null;
     }
   }
 }

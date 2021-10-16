eb82b40e38afa2bd52a9a8ae61b7e756531932fe
Tim <harvard90873@gmail.com>
2021-10-16 17:23:55 +0200

    server: implement discord oauth resolver?

--- packages/server/src/resolvers/UserResolvers.ts
+++ packages/server/src/resolvers/UserResolvers.ts
@@ -10,8 +10,10 @@ import {
 } from "type-graphql";
 import { notAuthenticatedErrorMessage } from "../constants/errorMessages";
 import { User } from "../entity/User";
+import { DiscordUser } from "../modules/discordUser";
 import { GoogleUser } from "../modules/googleUser";
 import { NetworkingContext } from "../types/NetworkingContext";
+import { getDiscordUserInfo } from "../utils/discordOAuth";
 import { getGoogleUserInfo } from "../utils/googleOAuth";
 import { loginOAuth } from "../utils/oauthLogin";
 
@@ -85,6 +87,54 @@ export class UserResolver {
         avatarUrl: userData.picture || undefined,
         provider: "Google",
         username: userData.given_name,
+      });
+    } catch (err) {}
+
+    user = await User.findOne({ where: { email } });
+
+    return loginOAuth(user!, req, res);
+  }
+
+  @Mutation(() => OAuthResponse)
+  async discordOAuth(
+    @Arg("code") code: string,
+    @Ctx() { res, req }: NetworkingContext
+  ): Promise<OAuthResponse> {
+    let userData: DiscordUser;
+    let email = "";
+
+    try {
+      userData = await getDiscordUserInfo(code);
+      if (!userData.email) {
+        throw new Error("No Email is linked with this discord account");
+      }
+      email = userData.email;
+    } catch (err) {
+      return {
+        user: null,
+      };
+    }
+
+    if (!email || !email.length || !validateEmailWithRegex(email)) {
+      throw new Error("Invalid email.");
+    }
+
+    let user: User | undefined = await User.findOne({ where: { email } });
+
+    if (user) {
+      if (user.provider === "Google") {
+        return loginOAuth(user, req, res);
+      } else {
+        throw new Error("Email already linked with another account.");
+      }
+    }
+
+    try {
+      await User.insert({
+        email,
+        avatarUrl: userData.avatar || undefined,
+        provider: "Google",
+        username: userData.username,
       });
     } catch (err) {}
 

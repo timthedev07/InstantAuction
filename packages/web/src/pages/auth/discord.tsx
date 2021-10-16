fa95d814d88bb27036ab7bfd1aed49945932f24f
Tim <harvard90873@gmail.com>
2021-10-16 17:37:22 +0200

    web: send code to server

---
+++ packages/web/src/pages/auth/discord.tsx
@@ -0,0 +1,35 @@
+import { useDiscordOAuthMutation } from "client-controllers";
+import { NextPage } from "next";
+import queryString from "query-string";
+import { useEffect, useState } from "react";
+
+const Discord: NextPage = () => {
+  const [state, setState] = useState<{}>();
+  const [googleOAuth] = useDiscordOAuthMutation();
+
+  useEffect(() => {
+    (async () => {
+      const urlParams = queryString.parse(window.location.search);
+      let code = urlParams.code as string;
+
+      if (!code) {
+        // TODO: handle error here
+      }
+
+      const response = await googleOAuth({
+        variables: {
+          code,
+        },
+      });
+      setState(response.data?.discordOAuth || {})
+    })();
+  }, []);
+
+  return (
+    <>
+      <pre>{JSON.stringify(state)}</pre>
+    </>
+  );
+};
+
+export default Discord;

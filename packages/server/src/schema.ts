efc1656f17c07b4b4e215c657e7b709ac1f75582
Tim <harvard90873@gmail.com>
2021-10-16 18:51:17 +0200

    server: include logout resolver in schema

--- packages/server/src/schema.ts
+++ packages/server/src/schema.ts
@@ -1,11 +1,17 @@
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/Hello";
 import { GetProfileResolver } from "./resolvers/users/GetProfile";
+import { LogoutResolver } from "./resolvers/users/Logout";
 import { OAuthResolver } from "./resolvers/users/OAuth";
 
 export const createSchema = () =>
   buildSchema({
-    resolvers: [HelloResolver, GetProfileResolver, OAuthResolver],
+    resolvers: [
+      HelloResolver,
+      GetProfileResolver,
+      OAuthResolver,
+      LogoutResolver,
+    ],
     authChecker: ({ context: { req } }) => {
       return !!req.session.userId;
     },

432dbec82ec5dd121cd493d5dc21eb587d1a83a9
Tim <harvard90873@gmail.com>
2021-10-16 15:53:23 +0200

    server: change to interface

--- packages/server/src/modules/discordUser.ts
+++ packages/server/src/modules/discordUser.ts
@@ -1,31 +1,15 @@
-import { Field, ArgsType } from "type-graphql";
-
-@ArgsType()
-export class DiscordUser {
-  @Field(() => String, { nullable: true })
+export interface DiscordUser {
   accent_color: string | null | undefined;
-  @Field(() => String, { nullable: true })
   avatar: string | null | undefined;
-  @Field(() => String, { nullable: true })
   banner: string | null | undefined;
-  @Field(() => String, { nullable: true })
   banner_color: string | null | undefined;
-  @Field()
   discriminator: string;
-  @Field(() => String, { nullable: true })
   email: string | null | undefined;
-  @Field()
   flags: number;
-  @Field()
   id: string;
-  @Field()
   locale: string;
-  @Field()
   mfa_enabled: boolean;
-  @Field()
   public_flags: number;
-  @Field()
   username: string;
-  @Field()
   verified: boolean;
 }

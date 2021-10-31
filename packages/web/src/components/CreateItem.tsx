67b981e8ceb48a092687c1c7b1cf7b2d8b45da35
Tim <harvard90873@gmail.com>
2021-10-31 22:26:38 +0100

    web: try creating item

--- packages/web/src/components/CreateItem.tsx
+++ packages/web/src/components/CreateItem.tsx
@@ -1,10 +1,11 @@
-import { useTestUploadLazyQuery } from "client-controllers";
-import { FC, useCallback, useState } from "react";
+import { useCreateItemMutation } from "client-controllers";
+import { FC, FormEventHandler, useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
 
 export const CreateItem: FC = ({}) => {
-  const [uploadFile] = useTestUploadLazyQuery();
+  const [createItem] = useCreateItemMutation();
   const [file, setFile] = useState<any>(null);
+  const [name, setName] = useState<string>("");
   const onDrop = useCallback(
     ([file]) => {
       setFile(file);
@@ -16,26 +17,37 @@ export const CreateItem: FC = ({}) => {
     onDrop
   });
 
-  const handleUpload = () => {
-    if (file) {
-      uploadFile({ variables: { file: file } });
-      alert("Upload request sent...");
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    if (file && name) {
+      try {
+        const result = await createItem({ variables: { name, picture: file } });
+        alert(result.data?.createItem.valueOf());
+      } catch(err: any) {
+        alert(err.graphQLErrors[0].message)
+      }
     }
   };
 
   return (
-    <div className="border-2 rounded p-8">
-      <h2 className="text-2xl">Upload image</h2>
+    <form onSubmit={handleSubmit} className="border-2 rounded p-8">
+      <h2 className="text-2xl">Add Item</h2>
+
+      <input
+        placeholder="Item Name"
+        value={name}
+        onChange={e => setName(e.target.value)}
+        className="bg-gray-500"
+      />
+
       <div {...getRootProps()} className="border-2 rounded p-8">
         <h3>{file ? file.path : "Drop your file here"}</h3>
         <input accept="image/*" {...getInputProps()} />
       </div>
-      <button
-        className="bg-cyan-600 text-white rounded p-4"
-        onClick={handleUpload}
-      >
+
+      <button className="bg-cyan-600 text-white rounded p-4" type="submit">
         Upload
       </button>
-    </div>
+    </form>
   );
 };

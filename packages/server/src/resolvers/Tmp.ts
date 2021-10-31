473e8a20fca9bd1b355889e916d6a403e46d577c
Tim <harvard90873@gmail.com>
2021-10-31 20:30:19 +0100

    server: try using the helper function

--- packages/server/src/resolvers/Tmp.ts
+++ packages/server/src/resolvers/Tmp.ts
@@ -1,11 +1,7 @@
 import { FileUpload, GraphQLUpload } from "graphql-upload";
 import { Arg, Query, Resolver } from "type-graphql";
 import { User } from "../entity/User";
-import { createWriteStream } from "fs";
-import { tmpdir } from "os";
-import { join } from "path";
-import { cyan, red } from "chalk";
-import { uploadToImgur } from "../utils/uploadToImgur";
+import { handleImageUpload } from "../utils/handleImageUpload";
 
 @Resolver()
 export class TmpResolvers {
@@ -20,32 +16,15 @@ export class TmpResolvers {
   }
 
   @Query(() => Boolean)
-  async testUpload(@Arg("file", () => GraphQLUpload)
-  {
-    createReadStream,
-    filename
-  }: FileUpload): Promise<boolean> {
-    const tempdir = tmpdir();
-    const imageBufferPath = join(tempdir, filename);
-    return await new Promise(async (resolve, reject) => {
-      createReadStream()
-        .pipe(createWriteStream(imageBufferPath))
-        .on("finish", async () => {
-          cyan(`File written to ${imageBufferPath}`);
-          try {
-            const link = await uploadToImgur(imageBufferPath);
-            console.log(link);
-            // do something with the link
-            resolve(true);
-          } catch (err) {
-            red("An error occurred while uploading to imgur: ", err);
-            reject(false);
-          }
-        })
-        .on("error", error => {
-          red("An error occurred while writing file:", error);
-          reject(false);
-        });
-    });
+  async testUpload(
+    @Arg("file", () => GraphQLUpload)
+    fileInfo: FileUpload
+  ): Promise<boolean> {
+    try {
+      await handleImageUpload(fileInfo);
+      return true;
+    } catch (err) {
+      return false;
+    }
   }
 }

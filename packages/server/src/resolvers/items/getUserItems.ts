fce5d911655c2fa27e1d850ac15e3b4346454fed
Tim <harvard90873@gmail.com>
2021-10-31 22:51:44 +0100

    server: new resolver

---
+++ packages/server/src/resolvers/items/getUserItems.ts
@@ -0,0 +1,43 @@
+import {
+  Ctx,
+  Field,
+  Int,
+  ObjectType,
+  Query,
+  Resolver,
+  UseMiddleware
+} from "type-graphql";
+import { NetworkingContext } from "../../types/NetworkingContext";
+import { isAuth } from "../../utils/isAuthMiddleware";
+import { getConnection } from "typeorm";
+import { Item } from "../../entity/Item";
+
+@ObjectType()
+class UserItemsResponse {
+  @Field(() => [Item])
+  items: Item[];
+
+  @Field(() => Int)
+  count: number;
+}
+
+@Resolver()
+export class GetUserItemsResolver {
+  @Query(() => UserItemsResponse)
+  @UseMiddleware(isAuth)
+  async getUserItems(@Ctx() { req }: NetworkingContext): Promise<
+    UserItemsResponse
+  > {
+    const userId = req.session.userId!;
+    const repo = getConnection().getRepository(Item);
+    const result = await repo
+      .createQueryBuilder("item")
+      .innerJoinAndSelect("item.owner", "owner")
+      .where("owner.id = :id", { id: userId })
+      .getManyAndCount();
+    return {
+      items: result[0],
+      count: result[1]
+    };
+  }
+}

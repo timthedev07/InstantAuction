2a7f7f5f380c2184060665b3bee12ce73b6ea733
Tim <harvard90873@gmail.com>
2021-11-01 19:49:56 +0100

    web: clean up material ui related stuff

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -3,22 +3,14 @@ import type { AppProps } from "next/app";
 import { Layout } from "../components/layout";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "client-controllers";
-import { createTheme, ThemeProvider } from "@material-ui/core";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  const darkTheme = createTheme({
-    palette: {
-      type: "dark",
-    },
-  });
   return (
-    <ThemeProvider theme={darkTheme}>
-      <ApolloProvider client={client}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ApolloProvider>
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ApolloProvider>
   );
 };
 

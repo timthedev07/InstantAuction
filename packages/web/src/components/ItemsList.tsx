bc85e6d857bfdac946aa6368a1917611293eef20
Tim <harvard90873@gmail.com>
2021-11-01 12:26:28 +0100

    web: add delete button to item component

--- packages/web/src/components/ItemsList.tsx
+++ packages/web/src/components/ItemsList.tsx
@@ -1,8 +1,9 @@
-import { useGetUserItemsQuery } from "client-controllers";
+import { useDeleteItemMutation, useGetUserItemsQuery } from "client-controllers";
 import { FC } from "react";
 
 export const ItemsList: FC = ({}) => {
   const {data, error, loading} = useGetUserItemsQuery();
+  const [deleteItem] = useDeleteItemMutation();
 
   return (
     <div>
@@ -10,6 +11,16 @@ export const ItemsList: FC = ({}) => {
         <div className="border-white border my-4 mx-1 p-2">
           <h2 className="text-3xl">{each.name}</h2>
           <img className="min-w-350 w-3/4 h-3/4 max-w-md" src={each.picture} />
+          <button onClick={async () => {
+            try {
+              const result = await deleteItem({variables: {itemId: each.id}});
+              if (result.data && result.data.deleteItem) {
+                alert("Item deleted");
+              }
+            } catch(err: any) {
+              alert(err.graphQLErrors[0].message);
+            }
+          }} className="danger-button">Delete Item</button>
         </div>
       ))}
     </div>

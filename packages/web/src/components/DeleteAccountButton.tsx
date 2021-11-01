0c8bd31e6e42fa7ebb8a079a7401431750cf176d
Tim <harvard90873@gmail.com>
2021-11-01 15:36:20 +0100

    web: correct email

--- packages/web/src/components/DeleteAccountButton.tsx
+++ packages/web/src/components/DeleteAccountButton.tsx
@@ -2,21 +2,25 @@ import { FC } from "react";
 import {
   createDeleteAccountOptions,
   useDeleteAccountMutation,
+  useMeQuery
 } from "client-controllers";
 
 export const DeleteAccountButton: FC = () => {
   const [deleteAccount] = useDeleteAccountMutation();
+  const { data, loading, error } = useMeQuery();
 
   return (
     <button
       className="border rounded p-2 px-4 text-center"
-      onClick={() =>
-        deleteAccount(
-          createDeleteAccountOptions({
-            email: "bot.rem.autogenerate@gmail.com",
-          })
-        )
-      }
+      onClick={() => {
+        if (!loading && !error && data && data.me) {
+          deleteAccount(
+            createDeleteAccountOptions({
+              email: data.me.email
+            })
+          );
+        }
+      }}
     >
       Delete account
     </button>

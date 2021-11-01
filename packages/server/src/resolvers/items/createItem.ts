a5ae9f322ac6896e65363c8c70b479e71ab902d5
Tim <harvard90873@gmail.com>
2021-11-01 13:04:35 +0100

    server: return data to update cache on client side

--- packages/server/src/resolvers/items/createItem.ts
+++ packages/server/src/resolvers/items/createItem.ts
@@ -1,29 +1,44 @@
 import { FileUpload, GraphQLUpload } from "graphql-upload";
 import { Arg, Ctx, Mutation, Resolver, UseMiddleware } from "type-graphql";
+import { getConnection } from "typeorm";
 import { Item } from "../../entity/Item";
 import { User } from "../../entity/User";
 import { NetworkingContext } from "../../types/NetworkingContext";
 import { handleImageUpload } from "../../utils/handleImageUpload";
 import { isAuth } from "../../utils/isAuthMiddleware";
+import { UserItemsResponse } from "./getUserItems";
 
 @Resolver()
 export class CreateItemResolver {
-  @Mutation(() => Boolean)
+  @Mutation(() => UserItemsResponse)
   @UseMiddleware(isAuth)
   async createItem(
     @Arg("name") name: string,
     @Arg("picture", () => GraphQLUpload)
     fileUpload: FileUpload,
     @Ctx() { req }: NetworkingContext
-  ) {
+  ): Promise<UserItemsResponse> {
     const userId = req.session.userId!;
     try {
       const picture = await handleImageUpload(fileUpload);
-      await Item.insert({ owner: await User.findOne(userId), name, picture });
+      await Item.insert({
+        name,
+        picture,
+        owner: await User.findOne(userId)
+      });
+      const repo = getConnection().getRepository(Item);
+      const queryResult = await repo
+        .createQueryBuilder("item")
+        .innerJoinAndSelect("item.owner", "owner")
+        .where("owner.id = :id", { id: userId })
+        .getManyAndCount();
 
-      return true;
+      return {
+        count: queryResult[1],
+        items: queryResult[0]
+      };
     } catch (err) {
-      return false;
+      throw err;
     }
   }
 }

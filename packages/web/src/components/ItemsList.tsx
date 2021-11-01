9b9d43160495900d6afcb1353d1cabc5733c2551
Tim <harvard90873@gmail.com>
2021-11-01 07:54:28 +0100

    web: remove owner text

--- packages/web/src/components/ItemsList.tsx
+++ packages/web/src/components/ItemsList.tsx
@@ -10,7 +10,6 @@ export const ItemsList: FC = ({}) => {
         <div className="border-white border my-4 mx-1 p-2">
           <h2 className="text-3xl">{each.name}</h2>
           <img className="min-w-350 w-3/4 h-3/4 max-w-md" src={each.picture} />
-          <span>Owned by <b><i>{each.owner.username}</i></b></span>
         </div>
       ))}
     </div>

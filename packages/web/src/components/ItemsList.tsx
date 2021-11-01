c603a425c7f5fd47f313dde76d35e3f8affc4c70
Tim <harvard90873@gmail.com>
2021-11-01 07:51:13 +0100

    web: adjust styles so it doesn't suck

--- packages/web/src/components/ItemsList.tsx
+++ packages/web/src/components/ItemsList.tsx
@@ -8,8 +8,8 @@ export const ItemsList: FC = ({}) => {
     <div>
       {loading ? "..." : error ? `${error}` : data?.getUserItems.items.map(each => (
         <div className="border-white border my-4 mx-1 p-2">
-          <h2>{each.name}</h2>
-          <img src={each.picture} />
+          <h2 className="text-3xl">{each.name}</h2>
+          <img className="min-w-350 w-3/4 h-3/4 max-w-md" src={each.picture} />
           <span>Owned by <b><i>{each.owner.username}</i></b></span>
         </div>
       ))}

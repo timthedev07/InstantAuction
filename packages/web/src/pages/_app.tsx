5ca60eed3816933cf38f9789c7677ffc014391bc
Tim <harvard90873@gmail.com>
2021-11-01 18:15:28 +0100

    web: material ui dark mode

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -3,14 +3,22 @@ import type { AppProps } from "next/app";
 import { Layout } from "../components/layout";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "client-controllers";
+import { createTheme, ThemeProvider } from "@material-ui/core";
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const darkTheme = createTheme({
+    palette: {
+      type: "dark",
+    },
+  });
   return (
-    <ApolloProvider client={client}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ApolloProvider>
+    <ThemeProvider theme={darkTheme}>
+      <ApolloProvider client={client}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ApolloProvider>
+    </ThemeProvider>
   );
 };
 

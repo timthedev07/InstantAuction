594de9b286ea57a1f0ab6d683369318731f2e097
Tim <harvard90873@gmail.com>
2021-11-01 13:10:01 +0100

    server: revert resolver

--- packages/server/src/resolvers/items/createItem.ts
+++ packages/server/src/resolvers/items/createItem.ts
@@ -1,44 +1,29 @@
 import { FileUpload, GraphQLUpload } from "graphql-upload";
 import { Arg, Ctx, Mutation, Resolver, UseMiddleware } from "type-graphql";
-import { getConnection } from "typeorm";
 import { Item } from "../../entity/Item";
 import { User } from "../../entity/User";
 import { NetworkingContext } from "../../types/NetworkingContext";
 import { handleImageUpload } from "../../utils/handleImageUpload";
 import { isAuth } from "../../utils/isAuthMiddleware";
-import { UserItemsResponse } from "./getUserItems";
 
 @Resolver()
 export class CreateItemResolver {
-  @Mutation(() => UserItemsResponse)
+  @Mutation(() => Boolean)
   @UseMiddleware(isAuth)
   async createItem(
     @Arg("name") name: string,
     @Arg("picture", () => GraphQLUpload)
     fileUpload: FileUpload,
     @Ctx() { req }: NetworkingContext
-  ): Promise<UserItemsResponse> {
+  ) {
     const userId = req.session.userId!;
     try {
       const picture = await handleImageUpload(fileUpload);
-      await Item.insert({
-        name,
-        picture,
-        owner: await User.findOne(userId)
-      });
-      const repo = getConnection().getRepository(Item);
-      const queryResult = await repo
-        .createQueryBuilder("item")
-        .innerJoinAndSelect("item.owner", "owner")
-        .where("owner.id = :id", { id: userId })
-        .getManyAndCount();
+      await Item.insert({ owner: await User.findOne(userId), name, picture });
 
-      return {
-        count: queryResult[1],
-        items: queryResult[0]
-      };
+      return true;
     } catch (err) {
-      throw err;
+      return false;
     }
   }
 }

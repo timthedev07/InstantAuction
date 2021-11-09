6926b6d8310c2e3843536a30cdb47f2d4f6cf9d9
Tim <harvard90873@gmail.com>
2021-11-09 18:27:11 +0100

    server: add checks before deletion

--- packages/server/src/resolvers/items/deleteItem.ts
+++ packages/server/src/resolvers/items/deleteItem.ts
@@ -1,12 +1,31 @@
-import { Resolver, Mutation, UseMiddleware, Arg, Int } from "type-graphql";
+import { Resolver, Mutation, UseMiddleware, Arg, Int, Ctx } from "type-graphql";
 import { Item } from "../../entity/Item";
+import { NetworkingContext } from "../../types/NetworkingContext";
 import { isAuth } from "../../utils/isAuthMiddleware";
 
 @Resolver()
 export class DeleteItemResolver {
   @Mutation(() => Boolean)
   @UseMiddleware(isAuth)
-  async deleteItem(@Arg("itemId", () => Int) itemId: number): Promise<boolean> {
+  async deleteItem(
+    @Arg("itemId", () => Int) itemId: number,
+    @Ctx() { req }: NetworkingContext
+  ): Promise<boolean> {
+    try {
+      const item = await Item.findOne({
+        where: {
+          id: itemId
+        },
+        relations: ["owner"]
+      });
+
+      if (item.owner.id !== req.session.userId) {
+        throw new Error("Unauthorized");
+      }
+    } catch (err) {
+      throw new Error("Invalid item");
+    }
+
     try {
       await Item.delete({ id: itemId });
       return true;

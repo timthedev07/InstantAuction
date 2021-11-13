9d9c3c470cfdfe66fac3f490e90852645cae9d40
Tim <harvard90873@gmail.com>
2021-11-13 20:25:02 +0100

    server: use constant

--- packages/server/src/resolvers/auctions/allAuctions.ts
+++ packages/server/src/resolvers/auctions/allAuctions.ts
@@ -1,4 +1,5 @@
 import { Resolver, Query, ObjectType, Field, Int } from "type-graphql";
+import { auctionExposedRelations } from "../../constants/exposed-relations";
 import { Auction } from "../../entity/Auction";
 
 @ObjectType()
@@ -15,7 +16,7 @@ export class AllAuctionsResolver {
   @Query(() => AllAuctionsResponse)
   async allAuctions(): Promise<AllAuctionsResponse> {
     const queryResult = await Auction.findAndCount({
-      relations: ["seller", "item"]
+      relations: auctionExposedRelations
     });
     // TODO: might need to add some filtering/sorting feature in the future
     return {

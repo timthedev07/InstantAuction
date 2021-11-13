17e92f36c06acc6b59c558b4239f19e18ad8bd34
Tim <harvard90873@gmail.com>
2021-11-13 21:21:08 +0100

    server: integrate new shit

--- packages/server/src/resolvers/items/getUserItems.ts
+++ packages/server/src/resolvers/items/getUserItems.ts
@@ -1,4 +1,5 @@
 import {
+  Arg,
   Ctx,
   Field,
   Int,
@@ -9,7 +10,6 @@ import {
 } from "type-graphql";
 import { NetworkingContext } from "../../types/NetworkingContext";
 import { isAuth } from "../../utils/isAuthMiddleware";
-import { getConnection } from "typeorm";
 import { Item } from "../../entity/Item";
 
 @ObjectType()
@@ -25,19 +25,21 @@ export class UserItemsResponse {
 export class GetUserItemsResolver {
   @Query(() => UserItemsResponse)
   @UseMiddleware(isAuth)
-  async getUserItems(@Ctx() { req }: NetworkingContext): Promise<
-    UserItemsResponse
-  > {
-    const userId = req.session.userId!;
-    const repo = getConnection().getRepository(Item);
-    const result = await repo
-      .createQueryBuilder("item")
-      .innerJoinAndSelect("item.owner", "owner")
-      .where("owner.id = :id", { id: userId })
-      .getManyAndCount();
+  async getUserItems(
+    @Ctx() { req }: NetworkingContext,
+    @Arg("excludeAuctionedOff") excludeAuctionedOff: boolean
+  ): Promise<UserItemsResponse> {
+    const [items, count] = await Item.findAndCount({
+      relations: ["owner"],
+      where: {
+        "owner.id": req.session.userId,
+        // filter based on the parameter
+        auctionedOff: excludeAuctionedOff || undefined
+      }
+    });
     return {
-      items: result[0],
-      count: result[1]
+      items,
+      count
     };
   }
 }

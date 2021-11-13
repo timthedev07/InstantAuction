38cde60fc1addab7aa443031b190a1b26b6ea1fe
Tim <harvard90873@gmail.com>
2021-11-13 13:26:12 +0100

    web: update list render

--- packages/web/src/components/AllAuctions.tsx
+++ packages/web/src/components/AllAuctions.tsx
@@ -2,24 +2,38 @@ import { useAllAuctionsQuery } from "client-controllers";
 import { FC } from "react";
 
 export const AllAuctions: FC = () => {
-  const {data, loading, error} = useAllAuctionsQuery();
-
-  const auctionsData = data?.allAuctions;
+  const { data, loading, error } = useAllAuctionsQuery();
 
   return (
     <div>
-      {loading ? "..." : error ? "Error fetching auctions" : <>
-        <h3 className="text-xl">{auctionsData!.count} Auctions</h3>
-        {auctionsData!.auctions.map(each => (
-          <li className="w-64 h-64 p-3 border border-white" key={each.id}>
-            <h3 className="text-xl">{each.title}</h3>
-            <i>Started by: {each.seller.username}</i>
-            <i>Status: {each.status}</i>
-            <p>{each.description}</p>
-            <img src={each.item.picture} />
-          </li>
-        ))}
-      </>}
+      {loading
+        ? "..."
+        : error
+        ? "Error fetching auctions"
+        : (() => {
+            if (!data) return;
+            const auctionsData = data.allAuctions;
+            return (
+              <>
+                <h3 className="text-xl">{auctionsData!.count} Auctions</h3>
+                {auctionsData!.auctions.map(each => (
+                  <li
+                    className="w-64 h-80 p-3 border border-white rounded-lg"
+                    key={each.id}
+                  >
+                    <h3 className="text-xl">{each.title}</h3>
+                    <i>
+                      Started by: {each.seller.username}
+                      <br />
+                      Status: {each.status}
+                    </i>
+                    <p>{each.description}</p>
+                    <img src={each.item.picture} className="w-auto h-28" />
+                  </li>
+                ))}
+              </>
+            );
+          })()}
     </div>
   );
 };

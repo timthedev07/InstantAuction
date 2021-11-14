e178335c789c11ee0dec1239208ec88f29e244af
Tim <harvard90873@gmail.com>
2021-11-14 20:41:26 +0100

    server: use another database for testing

--- packages/server/src/test-utils/testConn.ts
+++ packages/server/src/test-utils/testConn.ts
@@ -1,9 +1,13 @@
 import { createConnection, getConnectionOptions } from "typeorm";
+import { __prod__ } from "../constants/prod";
 
 export const getTestConnection = async (drop: boolean = false) => {
   return createConnection({
     ...(await getConnectionOptions()),
     dropSchema: drop,
-    synchronize: drop
+    synchronize: drop,
+    database: (__prod__
+      ? process.env.HP_DATABASE
+      : "InstantAuction.test") as any
   });
 };

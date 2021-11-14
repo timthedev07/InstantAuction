fee83eff40661e05b330f96a3ed3a898e534fa4f
Tim <harvard90873@gmail.com>
2021-11-14 20:06:50 +0100

    server: update tsjest config

--- packages/server/jest.config.js
+++ packages/server/jest.config.js
@@ -1,5 +1,10 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testRegex: "(/src/tests/.*|(\\.|/)(test|spec))\\.[jt]s?$",
+  moduleFileExtensions: ["ts", "js"],
+  maxWorkers: 1,
+  cache: true,
+  cacheDirectory: ".jest-cache"
 };

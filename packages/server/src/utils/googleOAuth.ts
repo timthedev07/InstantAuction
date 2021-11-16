23a4da61519483a2b900460093ab4cbfed688dbe
Tim <harvard90873@gmail.com>
2021-11-16 19:29:17 +0100

    server: correct json() function usage

--- packages/server/src/utils/googleOAuth.ts
+++ packages/server/src/utils/googleOAuth.ts
@@ -1,9 +1,10 @@
 import { FRONTEND } from "shared";
 import { GoogleUser } from "../modules/googleUser";
+import fetch from "node-fetch";
 
 const getAccessTokenFromCode = async (code: string) => {
   try {
-    const { json } = await fetch(`https://oauth2.googleapis.com/token`, {
+    const response = await fetch(`https://oauth2.googleapis.com/token`, {
       method: "post",
       body: JSON.stringify({
         client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
@@ -14,7 +15,7 @@ const getAccessTokenFromCode = async (code: string) => {
       }),
     });
 
-    return (await json() as any)?.access_token || "";
+    return (await response.json() as any)?.access_token || "";
   } catch (err) {
     console.log("something unexpected happened at function getAccessTokenFromCode")
     return "";
@@ -25,13 +26,13 @@ export const getGoogleUserInfo = async (code: string): Promise<GoogleUser> => {
   const accessToken: string = await getAccessTokenFromCode(code);
 
   try {
-    const { json } = await fetch("https://www.googleapis.com/oauth2/v2/userinfo", {
+    const response = await fetch("https://www.googleapis.com/oauth2/v2/userinfo", {
       method: "get",
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
     });
-    return (await json()) as GoogleUser;
+    return (await response.json()) as GoogleUser;
   } catch (err) {
     console.log("something unexpected happened at function getGoogleUserInfo")
     throw new Error("Error getting Google user info.")

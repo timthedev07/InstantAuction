531f73dbe16d55bdc4e1c32095e4f52e827e91ae
Tim <65774333+timthedev07@users.noreply.github.com>
2021-11-16 10:01:10 +0100

    server: remove extra logging

--- packages/server/src/test-utils/testConn.ts
+++ packages/server/src/test-utils/testConn.ts
@@ -1,10 +1,6 @@
 import { createConnection, getConnectionOptions } from "typeorm";
-import { __prod__, __test__ } from "../constants/prod";
 
 export const getTestConnection = async (drop: boolean = false) => {
-  if (__test__) {
-    console.log(await getConnectionOptions());
-  }
   return createConnection({
     ...(await getConnectionOptions()),
     dropSchema: drop,

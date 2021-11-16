245481109d96239b26048aece17aef214bff067b
Tim <harvard90873@gmail.com>
2021-11-16 18:12:36 +0100

    server: distinguish between local test and actions

--- packages/server/ormconfig.ts
+++ packages/server/ormconfig.ts
@@ -1,6 +1,6 @@
 const MODE = process.env.NODE_ENV;
 const __prod__ = MODE === "production";
-const __test__ = MODE === "test";
+const __cicd__ = process.env.TEST_MODE === "ci-cd";
 
 const basedir = __prod__ ? "dist" : "src";
 const fileType = __prod__ ? "js" : "ts";
@@ -9,21 +9,25 @@ const PG_PORT = process.env.POSTGRES_PORT;
 
 module.exports = {
   type: "postgres",
-  host: __test__ ? process.env.POSTGRES_HOST : __prod__ ? process.env.HP_HOST : "localhost",
-  port: __test__ && PG_PORT ? parseInt(PG_PORT) : 5432,
+  host: __cicd__
+    ? process.env.POSTGRES_HOST
+    : __prod__
+    ? process.env.HP_HOST
+    : "localhost",
+  port: __cicd__ && PG_PORT ? parseInt(PG_PORT) : 5432,
   username: __prod__
     ? process.env.HP_USERNAME
-    : __test__
+    : __cicd__
     ? "postgres"
     : process.env.POSTGRES_USERNAME,
   password: __prod__
     ? process.env.HP_PASSWORD
-    : __test__
+    : __cicd__
     ? "postgres"
     : process.env.POSTGRES_PASSWORD,
   database: __prod__
     ? process.env.HP_DATABASE
-    : __test__
+    : __cicd__
     ? "postgres"
     : "InstantAuction.local",
   entities: [`${basedir}/entity/**/*.${fileType}`],

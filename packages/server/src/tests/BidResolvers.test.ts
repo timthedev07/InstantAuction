4f41975b5574af39d6d28349c7caae89d90334c0
Tim <harvard90873@gmail.com>
2021-11-18 22:08:32 +0100

    server: bid resolvers tests

--- packages/server/src/tests/BidResolvers.test.ts
+++ packages/server/src/tests/BidResolvers.test.ts
@@ -1,1 +1,46 @@
-export const testBidResolvers = () => {};
+import {
+  alreadyParticipating,
+  notYourOwnAuctionMessage
+} from "../resolvers/bids/createBid";
+import { accessGraphqlErrorMessage } from "../test-utils/accessGraphqlError";
+import { callGraphql } from "../test-utils/callGraphql";
+import { auctionId } from "./AuctionResolvers.test";
+import { user, user2 } from "./index.test";
+import { items } from "./ItemResolvers.test";
+import { createBidSource } from "./sources";
+
+export const testBidResolvers = () => {
+  describe("Create Bid Resolver", () => {
+    it("rejects bid from the auction seller", async () => {
+      const result = await callGraphql({
+        source: createBidSource,
+        userId: user.id,
+        variableValues: {
+          itemId: items[1].id,
+          auctionId
+        }
+      });
+
+      expect(result.errors.length).toBeGreaterThan(0);
+      expect(accessGraphqlErrorMessage(result.errors)).toBe(
+        notYourOwnAuctionMessage
+      );
+    });
+
+    it("rejects an item already participating in another auction", async () => {
+      const result = await callGraphql({
+        source: createBidSource,
+        userId: user2.id,
+        variableValues: {
+          itemId: items[0].id,
+          auctionId
+        }
+      });
+
+      expect(result.errors.length).toBeGreaterThan(0);
+      expect(accessGraphqlErrorMessage(result.errors)).toBe(
+        alreadyParticipating
+      );
+    });
+  });
+};

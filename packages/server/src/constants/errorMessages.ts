ab69f693741e1e63c983413b90bc8d04b20519f1
Tim <harvard90873@gmail.com>
2021-11-23 07:48:10 +0100

    server: update error messages

--- packages/server/src/constants/errorMessages.ts
+++ packages/server/src/constants/errorMessages.ts
@@ -1,10 +1,11 @@
 export const notAuthenticatedErrorMessage = "Not authenticated";
 export const unauthorizedErrorMessage = "Unauthorized";
 export const invalidItem = "Invalid Item";
-export const notYourOwnAuctionMessage = "You can't bid at your own auction.";
+export const notYourOwnAuctionMessage = "You can't bid at your own auction";
 export const alreadyParticipating =
   "Item is already participating in an auction";
-export const cannotRebid = "You cannot rebid.";
+export const cannotRebid = "You cannot rebid";
 export const auctionClosed = "Auction Closed";
 export const invalidAuction = "Invalid Auction";
-export const invalidWinningBidId = "Invalid winning bid id";
+export const invalidWinningBidId = "Invalid winning bid";
+export const invalidBidId = "Invalid bid";

0a91887661199da08f63b8f47f79ae238367e947
Tim <harvard90873@gmail.com>
2021-11-24 21:45:45 +0100

    server: optimize test user generation

--- packages/server/src/tests/index.test.ts
+++ packages/server/src/tests/index.test.ts
@@ -8,42 +8,36 @@ import {
 } from "./AuctionResolvers.test";
 import { testItemResolvers } from "./ItemResolvers.test";
 import { testBidResolvers } from "./BidResolvers.test";
+import faker from "faker";
 
 export let connection: Connection;
 export let users: User[];
+
+const getFakeUser = () => {
+  return {
+    avatarUrl: "https://avatars.githubusercontent.com/u/65774333?v=4",
+    email: faker.internet.email(),
+    externalId: faker.internet.password(),
+    provider: "Google",
+    username: faker.internet.userName(),
+  };
+};
 
 // register a user to pass auth check
 beforeAll(async () => {
   const result = await getTestConnection();
   connection = result[0];
 
-  const { raw } = await User.insert({
-    avatarUrl: "https://avatars.githubusercontent.com/u/65774333?v=4",
-    email: "jeff@jeff.org",
-    externalId: "333",
-    provider: "Google",
-    username: "Jeff",
-  });
-  const { raw: raw1 } = await User.insert({
-    avatarUrl: "https://avatars.githubusercontent.com/u/87135844?v=4",
-    email: "chris@white.com",
-    externalId: "194817249812",
-    provider: "Discord",
-    username: "i.am.entrepreneur",
-  });
-  const { raw: raw2 } = await User.insert({
-    avatarUrl: "https://avatars.githubusercontent.com/u/87135844?v=4",
-    email: "john@carlson.com",
-    externalId: "iamjeffbezos",
-    provider: "Google",
-    username: "JEFFZEBOSISHERE",
-  });
+  const { raw } = await User.insert(getFakeUser());
+  const { raw: raw1 } = await User.insert(getFakeUser());
+  const { raw: raw2 } = await User.insert(getFakeUser());
 
   users = [
     await User.findOne(raw[0].id),
     await User.findOne(raw1[0].id),
     await User.findOne(raw2[0].id),
   ];
+
   setAuctionCreator(users[0]);
 });
 

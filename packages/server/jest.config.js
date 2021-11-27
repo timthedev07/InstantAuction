808833553f8099168588e9944abbadbab6de322d
Tim <harvard90873@gmail.com>
2021-11-27 15:25:19 +0100

    server: specify jest sequencer

--- packages/server/jest.config.js
+++ packages/server/jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testRegex: "(/src/tests/index.(test|spec))\\.ts?$",
+  testRegex: "(/src/tests/.*|(\\.|/)(test|spec))\\.ts?$",
   moduleFileExtensions: ["ts", "js"],
   maxWorkers: 1,
   cache: true,
@@ -11,4 +11,5 @@ module.exports = {
     color: "cyan",
     name: "GraphQL Resolvers Tests",
   },
+  testSequencer: "./jest-sequencer.js",
 };

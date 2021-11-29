35dd5e40fb3234d02eb9a1c7dfa231786f096bd3
Tim <harvard90873@gmail.com>
2021-11-29 21:21:49 +0100

    server: actually no

--- packages/server/src/redis.ts
+++ packages/server/src/redis.ts
@@ -3,13 +3,9 @@ import { createClient } from "redis";
 const ON_DOCKER = process.env.ON_DOCKER === "true";
 
 export const redisClient = createClient(
-  ON_DOCKER
+  process.env.NODE_ENV === "production"
     ? {
-        host: "host.docker.internal",
-      }
-    : process.env.NODE_ENV === "production"
-    ? {
-        host: process.env.REDIS_HOST!,
+        host: ON_DOCKER ? "host.docker.internal" : process.env.REDIS_HOST!,
         port: parseInt(process.env.REDIS_PORT!),
         password: process.env.REDIS_PASSWORD!,
       }

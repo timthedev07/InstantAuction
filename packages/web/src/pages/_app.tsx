b28fabaf7d27e57bd5e4158f7a60d381c67e7873
Tim <harvard90873@gmail.com>
2021-12-01 20:52:46 +0100

    web: wrap with react icon context

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -3,14 +3,17 @@ import type { AppProps } from "next/app";
 import { Layout } from "../components/layout";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "client-controllers";
+import { IconContext } from "react-icons/lib";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <ApolloProvider client={client as any}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ApolloProvider>
+    <IconContext.Provider value={{ color: "white", size: "30px" }}>
+      <ApolloProvider client={client as any}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ApolloProvider>
+    </IconContext.Provider>
   );
 };
 

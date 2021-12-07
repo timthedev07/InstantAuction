23e577f78b0e365d01f6615f46875b9f584be9c7
Tim <harvard90873@gmail.com>
2021-12-07 11:09:02 +0100

    web: new page

---
+++ packages/web/src/pages/auctions.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+import { AllAuctions } from "../components/AllAuctions";
+import { withApollo } from "../utils/withApollo";
+
+const Auctions: NextPage = () => {
+  return (
+    <div>
+      <AllAuctions />
+    </div>
+  );
+};
+
+export default withApollo({ ssr: true })(Auctions);

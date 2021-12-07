075f913e8a21771d494d4a1fc75c7b7068bdfbd0
Tim <harvard90873@gmail.com>
2021-12-07 22:08:59 +0100

    server: routes for twitter oauth

---
+++ packages/server/src/routes/auth.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import passport from "passport";
+import { FRONTEND } from "../constants/app";
+
+export const authRouter = Router();
+
+authRouter.get("/auth/twitter", passport.authenticate("twitter"));
+
+authRouter.get(
+  "/auth/twitter/callback",
+  passport.authenticate("twitter", {
+    failureRedirect: `${FRONTEND}/login?err="Failed to login with twitter."`,
+    session: false,
+  }),
+  async (_, res) => {
+    // Successful authentication, redirect home.
+    res.redirect("/");
+  }
+);

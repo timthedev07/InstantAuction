45cb64ed876174ba077b0763339b2a82dfdbb76c
Tim <harvard90873@gmail.com>
2021-12-07 11:08:47 +0100

    web: remove apollo context

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -1,18 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "../components/layout";
-import { ApolloProvider } from "@apollo/client";
-import { client } from "client-controllers";
 import { IconContext } from "@react-icons/all-files/lib";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <IconContext.Provider value={{ color: "white", size: "30px" }}>
-      <ApolloProvider client={client as any}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ApolloProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </IconContext.Provider>
   );
 };

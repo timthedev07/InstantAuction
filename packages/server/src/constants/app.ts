9dcc11578b70f213aa17afb580921adac8b38a52
Tim <harvard90873@gmail.com>
2021-12-07 22:09:09 +0100

    server: add another constant

--- packages/server/src/constants/app.ts
+++ packages/server/src/constants/app.ts
@@ -6,3 +6,7 @@ export const FRONTEND =
     ? "https://instantauction.vercel.app"
     : "http://localhost:3000";
 export const PLAYGROUND = "https://studio.apollographql.com";
+export const BACKEND =
+  process.env.NODE_ENV === "production"
+    ? "https://instantauction.herokuapp.com"
+    : "http://localhost:4000";

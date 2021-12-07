683ac34925aafb73d33d3421f6e4eb375d26532e
Tim <harvard90873@gmail.com>
2021-12-07 16:33:59 +0100

    web: add vector art img in

--- packages/web/src/pages/login.tsx
+++ packages/web/src/pages/login.tsx
@@ -4,9 +4,11 @@ const Login: NextPage = () => {
   return (
     <>
       <div className="flex justify-center items-center h-70vh">
-        <div className="flex bg-neutral-700 rounded-lg w-9/12 h-5/6 min-w-max">
+        <div className="flex bg-neutral-700 rounded-lg w-9/12 h-9/10 min-w-max">
           <div className="bg-transparent w-full md:w-5/12 lg:w-2/4" />
-          <div className="bg-primary-300 hidden rounded-r-lg md:w-7/12 md:block lg:block lg:w-2/4" />
+          <div className="bg-login-image-bg hidden rounded-r-lg md:w-7/12 md:flex lg:flex lg:w-2/4 items-center">
+            <img src="/images/login.jpg" alt="login" />
+          </div>
         </div>
       </div>
     </>

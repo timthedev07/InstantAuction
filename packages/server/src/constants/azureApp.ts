9d7860e8a90ecb59693f0598a72905c135582dd5
Tim <harvard90873@gmail.com>
2021-12-08 20:50:35 +0100

    server: add azure app config

---
+++ packages/server/src/constants/azureApp.ts
@@ -0,0 +1,18 @@
+import { LogLevel, Configuration } from "@azure/msal-node";
+
+export const config: Configuration = {
+  auth: {
+    clientId: process.env.AZURE_CLIENT_ID,
+    authority: "https://login.microsoftonline.com/common",
+    clientSecret: process.env.AZURE_CLIENT_SECRET,
+  },
+  system: {
+    loggerOptions: {
+      loggerCallback(_, message, __) {
+        console.log(message);
+      },
+      piiLoggingEnabled: false,
+      logLevel: LogLevel.Verbose,
+    },
+  },
+};

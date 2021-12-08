df229efc90f0ad8c9ac7b4322e2e7281373d7abb
Tim <harvard90873@gmail.com>
2021-12-08 21:27:33 +0100

    client-controller: options for the new mutation

---
+++ packages/client-controllers/src/hook-options/microsoftOAuthOptions.ts
@@ -0,0 +1,24 @@
+import {
+  MeDocument,
+  MeQuery,
+  MicrosoftOAuthMutationOptions,
+  MicrosoftOAuthMutationVariables
+} from "../generated/graphql";
+
+export const createGoogleOAuthOptions = (
+  variables: MicrosoftOAuthMutationVariables
+): MicrosoftOAuthMutationOptions => {
+  return {
+    variables,
+    update: (store, { data }) => {
+      if (!data || !data.microsoftOAuth) return;
+      store.writeQuery<MeQuery>({
+        query: MeDocument,
+        data: {
+          __typename: "Query",
+          me: data.microsoftOAuth.user
+        }
+      });
+    }
+  };
+};

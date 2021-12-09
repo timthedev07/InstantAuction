dfd487d0a6061c236c48957195707b004c183d39
Tim <harvard90873@gmail.com>
2021-12-09 20:45:32 +0100

    server: clean up

--- packages/server/src/utils/oauthLogin.ts
+++ packages/server/src/utils/oauthLogin.ts
@@ -1,16 +1,9 @@
 import { User } from "../entity/User";
-import { Request, Response } from "express";
+import { Request } from "express";
 import { OAuthResponse } from "../types/OAuthResponse";
 
-export const loginOAuth = (
-  user: User,
-  request: Request,
-  response: Response
-) => {
-  // TODO: Implement login logic with session
+export const loginOAuth = (user: User, request: Request) => {
   request.session.userId = user.id;
-
-  response;
 
   return {
     user,

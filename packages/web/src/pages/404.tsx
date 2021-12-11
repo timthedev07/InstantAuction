6c3f0da44fe69a200fdb68acf854eba3ddc523d4
Tim <harvard90873@gmail.com>
2021-12-11 21:21:25 +0100

    web: add 404 head

--- packages/web/src/pages/404.tsx
+++ packages/web/src/pages/404.tsx
@@ -1,32 +1,43 @@
 import { NextPage } from "next";
 import { useRouter } from "next/dist/client/router";
+import { getHeadForPage } from "../utils/getHeadForPage";
 
 const NotFound: NextPage = () => {
-  const { back } = useRouter();
+  const { back, asPath } = useRouter();
 
   return (
-    <div className="flex items-center justify-center md:justify-end relative h-full">
-      <img
-        src="/images/404-bg.jpg"
-        className="min-h-700 h-screen w-full object-cover object-left"
-      />
-      <div className="w-4/12 min-w-[310px] h-[500px] bg-[#2e353577] z-10 absolute rounded-lg mr-[7%] shadow-lg flex flex-col items-center">
-        <img src="/images/404-heading.svg" className="w-11/12 mt-12 md:mt-6" />
-        <h5 className="text-center text-gray-300">
-          Oops, you seem to be lost.
-        </h5>
-        <div className="w-full h-2/6 flex justify-center items-center">
-          <button
-            className="secondary-button z-50"
-            onClick={() => {
-              back();
-            }}
-          >
-            Go Back
-          </button>
+    <>
+      {getHeadForPage({
+        description: "Page Not Found",
+        path: asPath,
+        title: "404"
+      })}
+      <div className="flex items-center justify-center md:justify-end relative h-full">
+        <img
+          src="/images/404-bg.jpg"
+          className="min-h-700 h-screen w-full object-cover object-left"
+        />
+        <div className="w-4/12 min-w-[310px] h-[500px] bg-[#2e353577] z-10 absolute rounded-lg mr-[7%] shadow-lg flex flex-col items-center">
+          <img
+            src="/images/404-heading.svg"
+            className="w-11/12 mt-12 md:mt-6"
+          />
+          <h5 className="text-center text-gray-300">
+            Oops, you seem to be lost.
+          </h5>
+          <div className="w-full h-2/6 flex justify-center items-center">
+            <button
+              className="secondary-button z-50"
+              onClick={() => {
+                back();
+              }}
+            >
+              Go Back
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 

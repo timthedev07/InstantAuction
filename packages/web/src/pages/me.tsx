227895b8cc777e54aaefca9b1de0af1c056fcd36
Tim <harvard90873@gmail.com>
2021-12-12 16:47:19 +0100

    web: basic profile

--- packages/web/src/pages/me.tsx
+++ packages/web/src/pages/me.tsx
@@ -1,17 +1,43 @@
-import { useMeQuery } from "client-controllers";
+import {
+  useMeQuery,
+  useUpdateEmailVisibilityMutation
+} from "client-controllers";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { LoginPage } from "../components/pages/LoginPage";
 import { getHeadForPage } from "../utils/getHeadForPage";
 import { withApollo } from "../utils/withApollo";
+import Switch from "@mui/material/Switch";
+import ReactTooltip from "react-tooltip";
+import { Stack, Typography } from "@mui/material";
+import { AiFillEye } from "@react-icons/all-files/ai/AiFillEye";
+import { AiFillLock } from "@react-icons/all-files/ai/AiFillLock";
+import { UserTabs } from "../components/UserTabs";
 
 const MePage: NextPage = () => {
   const { query, isReady, asPath } = useRouter();
   const { data, loading } = useMeQuery({
-    fetchPolicy: "cache-first",
     ssr: false
   });
+  const [checked, setChecked] = useState(false);
+  const [updateEmailVisibility] = useUpdateEmailVisibilityMutation();
+
+  useEffect(() => {
+    if (!data || !data.me) return;
+    setChecked(data.me.emailPublic);
+  }, [data]);
+
+  const handleChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newVal = event.target.checked;
+    setChecked(newVal);
+
+    await updateEmailVisibility({
+      variables: {
+        newEmailPublic: newVal
+      }
+    });
+  };
 
   useEffect(() => {
     if (!isReady) return;
@@ -34,12 +60,37 @@ const MePage: NextPage = () => {
         path: asPath,
         title: `user - ${data.me.username}`
       })}
+      <ReactTooltip key={checked ? "dfdf" : "svsv"} />
       <div>
-        <div>
+        <section className="w-11/12 flex items-start flex-col p-8">
           {/* User information component */}
-          <h1>{data.me.username}</h1>
-        </div>
-        <div>{/* Tabs */}</div>
+          <div className="flex justify-start gap-4 items-center my-5">
+            <img
+              src={data.me.avatarUrl}
+              className="rounded-full w-24 h-24 object-cover"
+            />
+            <h2>{data.me.username}</h2>
+          </div>
+          <div className="rounded-md border border-gray-500 p-3 flex justify-around gap-8 items-center bg-neutral-800">
+            <h5>{data.me.email}</h5>
+            <Stack direction="row" alignItems="center">
+              <Typography>
+                <AiFillLock />
+              </Typography>
+              <Switch
+                key={checked ? "PublicSwitch" : "PrivateSwitch"}
+                data-tip={checked ? "Public" : "Private"}
+                checked={checked}
+                onChange={handleChange}
+                color="info"
+              />
+              <Typography>
+                <AiFillEye />
+              </Typography>
+            </Stack>
+          </div>
+        </section>
+        <UserTabs />
       </div>
     </>
   );

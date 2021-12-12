775ce538f9350391e220f40f64b65689efad3653
Tim <harvard90873@gmail.com>
2021-12-12 11:44:13 +0100

    web: material ui dark theme

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -2,14 +2,23 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "../components/layout";
 import { IconContext } from "@react-icons/all-files/lib";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <IconContext.Provider value={{ color: "white", size: "30px" }}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </IconContext.Provider>
+    <ThemeProvider theme={darkTheme}>
+      <IconContext.Provider value={{ color: "white", size: "30px" }}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </IconContext.Provider>
+    </ThemeProvider>
   );
 };
 

fe4c7c2bdb8207114a4a5e1748caf774c5040c55
Tim <harvard90873@gmail.com>
2021-12-12 21:47:32 +0100

    web: building tabs

--- packages/web/src/components/UserTabs.tsx
+++ packages/web/src/components/UserTabs.tsx
@@ -1,9 +1,69 @@
+import { useAuctionsOwnedQuery, useItemsOwnedQuery } from "client-controllers";
 import { FC } from "react";
+import { Auction } from "./Auction";
 
 interface UserTabsProps {
   tab?: string;
 }
 
-export const UserTabs: FC<UserTabsProps> = ({}) => {
-  return <section className="">{/* Tabs */}</section>;
+type TabType = "auctions-owned" | "items-owned" | "bids";
+
+const AuctionsTab = () => {
+  const { data, loading, error } = useAuctionsOwnedQuery();
+
+  return (
+    <article>
+      <ul>
+        {loading
+          ? "..."
+          : !data
+          ? JSON.stringify(error)
+          : data.auctionsOwned.auctions.map(each => <Auction auction={each} />)}
+      </ul>
+    </article>
+  );
+};
+
+const ItemsTab = () => {
+  const { data, loading, error } = useItemsOwnedQuery({
+    variables: {
+      excludeAuctionedOff: false
+    }
+  });
+
+  return (
+    <article>
+      <ul>
+        {loading
+          ? "..."
+          : !data
+          ? JSON.stringify(error)
+          : data.itemsOwned.items.map(each => <li>{each.name}</li>)}
+      </ul>
+    </article>
+  );
+};
+
+export const UserTabs: FC<UserTabsProps> = ({ tab }) => {
+  return (
+    <section className="">
+      {(() => {
+        switch (tab as TabType) {
+          case "items-owned": {
+            return <ItemsTab />;
+          }
+          case "bids": {
+            return <></>;
+          }
+          default: {
+            return <AuctionsTab />;
+          }
+        }
+      })()}
+    </section>
+  );
+};
+
+UserTabs.defaultProps = {
+  tab: "auctions-owned"
 };

1936f4e0c2d8bd96acd39adde7daba74eabf4391
Tim <harvard90873@gmail.com>
2021-12-12 16:47:27 +0100

    web: new component

---
+++ packages/web/src/components/UserTabs.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface UserTabsProps {
+  tab?: string;
+}
+
+export const UserTabs: FC<UserTabsProps> = ({}) => {
+  return <section className="">{/* Tabs */}</section>;
+};

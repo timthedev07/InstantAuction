088861a630a88866d55ab06c4f2fa0ae4f277e73
Tim <harvard90873@gmail.com>
2021-12-12 21:47:06 +0100

    server: fix not fetching exposed relations

--- packages/server/src/resolvers/auctions/auctionsOwned.ts
+++ packages/server/src/resolvers/auctions/auctionsOwned.ts
@@ -1,4 +1,5 @@
 import { Resolver, Query, UseMiddleware, Ctx } from "type-graphql";
+import { auctionExposedRelations } from "../../constants/exposed-relations";
 import { Auction } from "../../entity/Auction";
 import { NetworkingContext } from "../../types/NetworkingContext";
 import { isAuth } from "../../utils/isAuthMiddleware";
@@ -12,7 +13,7 @@ export class AuctionsOwnedResolver {
     AuctionsResponse
   > {
     const [auctions, count] = await Auction.findAndCount({
-      relations: ["seller"],
+      relations: auctionExposedRelations,
       where: {
         seller: { id: req.session.userId },
       },

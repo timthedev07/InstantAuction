a9c42c432534a3715905424742c5baecebae6426
Tim <harvard90873@gmail.com>
2021-12-12 12:15:31 +0100

    server: add a separate middleware instead

--- packages/server/src/utils/isAuthMiddleware.ts
+++ packages/server/src/utils/isAuthMiddleware.ts
@@ -1,18 +1,37 @@
 import { MiddlewareFn } from "type-graphql";
 import { notAuthenticatedErrorMessage } from "../constants/errorMessages";
+import { User } from "../entity/User";
 import { NetworkingContext } from "../types/NetworkingContext";
 
 /**
  * This middleware function checks whether the user is authenticated
  * according to the context variable.
- *
- * Tries to find the user in the database, and if there's no such user, an error is thrown.
- * @returns
  */
 export const isAuth: MiddlewareFn<NetworkingContext> = ({ context }, next) => {
   const userId = context.req.session.userId;
 
   if (!userId) {
+    throw new Error(notAuthenticatedErrorMessage);
+  }
+
+  return next();
+};
+
+/**
+ * This middleware function checks whether the user is authenticated
+ * according to the context variable.
+ *
+ * **Tries to find the user in the database, and if there's no such user, an error is thrown.**
+ *
+ * This middleware is suitable when you don't need to fetch the user entity in your resolver again, i.e. only needs the userId
+ */
+export const isAuthStrict: MiddlewareFn<NetworkingContext> = async (
+  { context },
+  next
+) => {
+  const user = await User.findOne(context.req.session.userId);
+
+  if (!user) {
     throw new Error(notAuthenticatedErrorMessage);
   }
 

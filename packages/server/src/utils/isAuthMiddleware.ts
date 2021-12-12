cd9d2de700ba7e17bbb42012ac9e754cebdb4c5b
Tim <harvard90873@gmail.com>
2021-12-12 12:08:57 +0100

    server: change auth checking strategy

--- packages/server/src/utils/isAuthMiddleware.ts
+++ packages/server/src/utils/isAuthMiddleware.ts
@@ -1,19 +1,23 @@
 import { MiddlewareFn } from "type-graphql";
 import { notAuthenticatedErrorMessage } from "../constants/errorMessages";
+import { User } from "../entity/User";
 import { NetworkingContext } from "../types/NetworkingContext";
 
 /**
  * This middleware function checks whether the user is authenticated
  * according to the context variable.
  *
- *
- *
+ * Tries to find the user in the database, and if there's no such user, an error is thrown.
  * @returns
  */
-export const isAuth: MiddlewareFn<NetworkingContext> = ({ context }, next) => {
+export const isAuth: MiddlewareFn<NetworkingContext> = async (
+  { context },
+  next
+) => {
   const userId = context.req.session.userId;
+  const user = await User.findOne(userId);
 
-  if (!userId) {
+  if (!user) {
     throw new Error(notAuthenticatedErrorMessage);
   }
 

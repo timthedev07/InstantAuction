abeca25435ba999a481f808760377ef381cc0bb2
Tim <harvard90873@gmail.com>
2021-12-18 13:37:25 +0100

    server: tweak ormconfig for heroku prod

--- packages/server/ormconfig.ts
+++ packages/server/ormconfig.ts
@@ -4,44 +4,36 @@ const MODE = process.env.NODE_ENV;
 const __prod__ = MODE === "production";
 const __cicd__ = process.env.TEST_MODE === "ci-cd";
 const __localtest__ = MODE === "test";
-const ON_DOCKER = process.env.ON_DOCKER === "true";
 
 const basedir = __prod__ ? "dist" : "src";
 const fileType = __prod__ ? "js" : "ts";
 
 const PG_PORT = process.env.POSTGRES_PORT;
 
+/**
+ * The production heroku pg url includes
+ *  - username
+ *  - password
+ *  - host
+ *  - port
+ *  - database
+ */
+
 module.exports = {
   type: "postgres",
-  host: __cicd__
-    ? process.env.POSTGRES_HOST
-    : __prod__
-    ? process.env.HP_HOST
-    : ON_DOCKER
-    ? "host.docker.internal"
-    : "localhost",
-  port: ON_DOCKER
-    ? parseInt(PG_PORT)
-    : __cicd__ && PG_PORT
-    ? parseInt(PG_PORT)
-    : 5432,
-  username: __prod__
-    ? process.env.HP_USERNAME
-    : __cicd__ || ON_DOCKER
-    ? "postgres"
-    : process.env.POSTGRES_USERNAME,
-  password: __prod__
-    ? process.env.HP_PASSWORD
-    : __cicd__ || ON_DOCKER
-    ? "postgres"
-    : process.env.POSTGRES_PASSWORD,
-  database: __prod__
-    ? process.env.HP_DATABASE
-    : __cicd__ || ON_DOCKER
-    ? "postgres"
-    : __localtest__
-    ? "InstantAuction.test"
-    : "InstantAuction.local",
+  ...(__prod__
+    ? { url: process.env.DATABASE_URL /* this env var is provided by heroku */ }
+    : {
+        host: __cicd__ ? process.env.POSTGRES_HOST : "localhost",
+        port: __cicd__ && PG_PORT ? parseInt(PG_PORT) : 5432,
+        username: __cicd__ ? "postgres" : process.env.POSTGRES_USERNAME,
+        password: __cicd__ ? "postgres" : process.env.POSTGRES_PASSWORD,
+        database: __cicd__
+          ? "postgres"
+          : __localtest__
+          ? "InstantAuction.test"
+          : "InstantAuction.local",
+      }),
   entities: [`${basedir}/entity/**/*.${fileType}`],
   migrations: [`${basedir}/migrations/**/*.${fileType}`],
   subscribers: [`${basedir}/subscriber/**/*.${fileType}`],

48e7995e39c447566fdf18032eb924021456e1e5
Tim <harvard90873@gmail.com>
2021-12-19 20:43:52 +0100

    client-controllers: new options

--- packages/client-controllers/src/hook-options/createItemOptions.ts
+++ packages/client-controllers/src/hook-options/createItemOptions.ts
@@ -1,6 +1,8 @@
 import {
   CreateItemMutationOptions,
   CreateItemMutationVariables,
+  CreateItemWithPictureUrlMutationOptions,
+  CreateItemWithPictureUrlMutationVariables,
   ItemsOwnedDocument,
   ItemsOwnedQuery
 } from "../generated/graphql";
@@ -24,6 +26,37 @@ export const createItemCreationOptions = (
         // if there are cached items
         count = cachedData.itemsOwned.count + 1;
         items = [...cachedData.itemsOwned.items, data.createItem];
+      }
+
+      store.writeQuery<ItemsOwnedQuery>({
+        query: ItemsOwnedDocument,
+        data: {
+          itemsOwned: { count, items, __typename: "UserItemsResponse" }
+        }
+      });
+    }
+  };
+};
+
+export const createItemCreationWithPictureUrlOptions = (
+  variables: CreateItemWithPictureUrlMutationVariables
+): CreateItemWithPictureUrlMutationOptions => {
+  return {
+    variables,
+    update: (store, { data }) => {
+      if (!data || !data.createItemWithPictureUrl) return;
+
+      const cachedData = store.readQuery<ItemsOwnedQuery>({
+        query: ItemsOwnedDocument
+      });
+
+      let count: number = 1;
+      let items = [data.createItemWithPictureUrl];
+
+      if (cachedData && cachedData.itemsOwned && cachedData.itemsOwned.count) {
+        // if there are cached items
+        count = cachedData.itemsOwned.count + 1;
+        items = [...cachedData.itemsOwned.items, data.createItemWithPictureUrl];
       }
 
       store.writeQuery<ItemsOwnedQuery>({

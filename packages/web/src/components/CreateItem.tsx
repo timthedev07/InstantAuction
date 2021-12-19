489d765fb2f2f06ea5e4a1ed949fdb22a72ad859
Tim <harvard90873@gmail.com>
2021-12-19 20:56:25 +0100

    web: add option to enter image url

--- packages/web/src/components/CreateItem.tsx
+++ packages/web/src/components/CreateItem.tsx
@@ -1,12 +1,23 @@
 import {
   createItemCreationOptions,
-  useCreateItemMutation
+  createItemCreationWithPictureUrlOptions,
+  useCreateItemMutation,
+  useCreateItemWithPictureUrlMutation
 } from "client-controllers";
 import { FC, FormEventHandler, useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField
+} from "@mui/material";
+import { VStack } from "./utils/Stack";
 
 export const CreateItem: FC = ({}) => {
   const [createItem] = useCreateItemMutation();
+  const [createItemWithPicUrl] = useCreateItemWithPictureUrlMutation();
   const [file, setFile] = useState<any>(null);
   const [name, setName] = useState<string>("");
   const onDrop = useCallback(
@@ -15,6 +26,8 @@ export const CreateItem: FC = ({}) => {
     },
     [setFile]
   );
+  const [mode, setMode] = useState<"file" | "url">("file");
+  const [picUrl, setPicUrl] = useState<string>("");
 
   const { getRootProps, getInputProps } = useDropzone({
     onDrop
@@ -22,14 +35,30 @@ export const CreateItem: FC = ({}) => {
 
   const handleSubmit: FormEventHandler<HTMLFormElement> = async e => {
     e.preventDefault();
-    if (file && name) {
-      try {
-        await createItem(createItemCreationOptions({ name, picture: file }));
-      } catch (err) {
-        alert((err as any).graphQLErrors[0].message);
+    if (mode === "file") {
+      if (file && name) {
+        try {
+          await createItem(createItemCreationOptions({ name, picture: file }));
+        } catch (err) {
+          alert((err as any).graphQLErrors[0].message);
+        }
+      } else {
+        alert("Please make sure all information is provided.");
       }
     } else {
-      alert("Please make sure all information is provided.");
+      if (picUrl !== "" /** && check pic url validity here */) {
+        try {
+          const res = await createItemWithPicUrl(
+            createItemCreationWithPictureUrlOptions({
+              name,
+              pictureUrl: picUrl
+            })
+          );
+        } catch (err) {
+          console.log(err);
+        }
+      } else {
+      }
     }
   };
 
@@ -37,17 +66,46 @@ export const CreateItem: FC = ({}) => {
     <form onSubmit={handleSubmit} className="border-2 rounded p-8">
       <h3>Add Item</h3>
 
-      <input
-        placeholder="Item Name"
-        value={name}
-        onChange={e => setName(e.target.value)}
-        className="bg-gray-500"
-      />
+      <VStack className="max-w-[400px] gap-4 mb-4">
+        <TextField
+          value={name}
+          onChange={e => setName(e.target.value)}
+          label="Item name"
+        />
+        <FormControl variant="filled">
+          <InputLabel>Image Format</InputLabel>
+          <Select
+            id="demo-simple-select"
+            value={mode}
+            label="Image Format"
+            onChange={e => {
+              setMode(e.target.value as any);
+            }}
+          >
+            <MenuItem value="url">URL</MenuItem>
+            <MenuItem value="file">File Upload</MenuItem>
+          </Select>
+        </FormControl>
 
-      <div {...getRootProps()} className="border-2 rounded p-8">
-        <h4>{file ? file.path : "Drop your file here"}</h4>
-        <input accept="image/*" {...getInputProps()} />
-      </div>
+        <div
+          {...getRootProps()}
+          className={`border-2 rounded p-8 ${
+            mode === "file" ? "block" : "hidden"
+          }`}
+        >
+          <h4>{file ? file.path : "Drop your file here"}</h4>
+          <input accept="image/*" {...getInputProps()} />
+        </div>
+        <TextField
+          label="Image Url"
+          value={picUrl}
+          onChange={e => setPicUrl(e.target.value)}
+          className={mode === "url" ? "block" : "hidden"}
+          variant="filled"
+        />
+      </VStack>
+
+      <div className="">{/* preview */}</div>
 
       <button className="cyan-button" type="submit">
         Add

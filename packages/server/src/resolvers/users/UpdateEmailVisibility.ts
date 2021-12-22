5f607cc127e5dd13419f0e992096c34fa9eaa496
Tim <harvard90873@gmail.com>
2021-12-22 14:00:51 +0100

    server: rate limit another resolver

--- packages/server/src/resolvers/users/UpdateEmailVisibility.ts
+++ packages/server/src/resolvers/users/UpdateEmailVisibility.ts
@@ -2,11 +2,17 @@ import { Resolver, Mutation, UseMiddleware, Ctx, Arg } from "type-graphql";
 import { User } from "../../entity/User";
 import { NetworkingContext } from "../../types/NetworkingContext";
 import { isAuthStrict } from "../../utils/isAuthMiddleware";
+import { rateLimit } from "../../utils/rateLimit";
 
 @Resolver()
 export class UpdateEmailVisibilityResolver {
   @Mutation(() => User)
   @UseMiddleware(isAuthStrict)
+  @UseMiddleware(
+    rateLimit(0, 3, {
+      errorMessage: "Slow down, you are editing too fast.",
+    })
+  )
   async updateEmailVisibility(
     @Ctx() { req }: NetworkingContext,
     @Arg("newEmailPublic") newEmailPublic: boolean

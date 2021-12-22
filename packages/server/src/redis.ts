285a57324db62924d37a74094ec9a34599ab2c96
Tim <harvard90873@gmail.com>
2021-12-22 12:25:03 +0100

    server: tweak redis client

--- packages/server/src/redis.ts
+++ packages/server/src/redis.ts
@@ -1,12 +1,8 @@
 import { createClient } from "redis";
 
-const ON_DOCKER = process.env.ON_DOCKER === "true";
-
 export const redisClient = createClient(
   process.env.NODE_ENV === "production"
     ? {
-        host: ON_DOCKER ? "host.docker.internal" : process.env.REDIS_HOST!,
-        port: parseInt(process.env.REDIS_PORT!),
         password: process.env.REDIS_PASSWORD!,
       }
     : undefined

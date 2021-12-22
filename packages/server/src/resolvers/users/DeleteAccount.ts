6d61acb1ba1f88a668f65084e85ae196cd6c701c
Tim <harvard90873@gmail.com>
2021-12-22 13:48:42 +0100

    server: rate limit resolver

--- packages/server/src/resolvers/users/DeleteAccount.ts
+++ packages/server/src/resolvers/users/DeleteAccount.ts
@@ -2,11 +2,13 @@ import { Arg, Ctx, Mutation, Resolver, UseMiddleware } from "type-graphql";
 import { User } from "../../entity/User";
 import { NetworkingContext } from "../../types/NetworkingContext";
 import { isAuth } from "../../utils/isAuthMiddleware";
+import { rateLimit } from "../../utils/rateLimit";
 
 @Resolver()
 export class DeleteAccountResolver {
   @Mutation(() => Boolean)
   @UseMiddleware(isAuth)
+  @UseMiddleware(rateLimit(0, 1, 60 * 3))
   async deleteAccount(
     @Arg("email") email: string,
     @Ctx() { req }: NetworkingContext

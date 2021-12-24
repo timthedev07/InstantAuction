52ad3f306f54cc72088165f78749ab80a01190b6
Tim <harvard90873@gmail.com>
2021-12-24 10:47:49 +0100

    server: tweak redis config for prod

--- packages/server/src/redis.ts
+++ packages/server/src/redis.ts
@@ -1,9 +1,23 @@
 import { createClient } from "redis";
+import { __prod__ } from "./constants/prod";
+
+const REDIS_URL = process.env.REDIS_URL;
+const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
+
+if (__prod__) {
+  if (!REDIS_URL) {
+    console.warn("Redis url not specified.");
+  }
+  if (!REDIS_PASSWORD) {
+    console.warn("Redis password not specified.");
+  }
+}
 
 export const redisClient = createClient(
-  process.env.NODE_ENV === "production"
+  __prod__
     ? {
-        password: process.env.REDIS_PASSWORD!,
+        url: REDIS_URL,
+        password: REDIS_PASSWORD,
       }
     : undefined
 );

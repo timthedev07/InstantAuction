68ccfb48a0ecaa30fa6857f9265f2355818957c6
Tim <harvard90873@gmail.com>
2021-12-24 16:51:42 +0100

    web: add chakra dark theme

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -2,23 +2,18 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "../components/layout";
 import { IconContext } from "@react-icons/all-files/lib";
-import { createTheme, ThemeProvider } from "@mui/material";
-
-const darkTheme = createTheme({
-  palette: {
-    mode: "dark",
-  },
-});
+import { ChakraProvider } from "@chakra-ui/react";
+import { chakraTheme } from "../utils/chakraTheme";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ChakraProvider theme={chakraTheme}>
       <IconContext.Provider value={{ color: "white", size: "30px" }}>
         <Layout>
           <Component {...pageProps} />
         </Layout>
       </IconContext.Provider>
-    </ThemeProvider>
+    </ChakraProvider>
   );
 };
 

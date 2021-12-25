cc7a31986a540c9a919a72180a65f0a089dd4d36
Tim <harvard90873@gmail.com>
2021-12-25 10:54:12 +0100

    client-controllers: parallel cache update

--- packages/client-controllers/src/hook-options/createItemOptions.ts
+++ packages/client-controllers/src/hook-options/createItemOptions.ts
@@ -4,7 +4,8 @@ import {
   CreateItemWithPictureUrlMutationOptions,
   CreateItemWithPictureUrlMutationVariables,
   ItemsOwnedDocument,
-  ItemsOwnedQuery
+  ItemsOwnedQuery,
+  ItemsOwnedQueryVariables
 } from "../generated/graphql";
 
 export const createItemCreationOptions = (
@@ -46,23 +47,83 @@ export const createItemCreationWithPictureUrlOptions = (
     update: (store, { data }) => {
       if (!data || !data.createItemWithPictureUrl) return;
 
-      const cachedData = store.readQuery<ItemsOwnedQuery>({
-        query: ItemsOwnedDocument
-      });
+      const cachedData = {
+        all: store.readQuery<ItemsOwnedQuery, ItemsOwnedQueryVariables>({
+          query: ItemsOwnedDocument,
+          variables: {
+            excludeAuctionedOff: false
+          }
+        }),
+        notAuctionedOff: store.readQuery<
+          ItemsOwnedQuery,
+          ItemsOwnedQueryVariables
+        >({
+          query: ItemsOwnedDocument,
+          variables: {
+            excludeAuctionedOff: true
+          }
+        })
+      };
 
-      let count: number = 1;
-      let items = [data.createItemWithPictureUrl];
+      let count = {
+        all: 1,
+        notAuctionedOff: 1
+      };
 
-      if (cachedData && cachedData.itemsOwned && cachedData.itemsOwned.count) {
+      let items = {
+        all: [data.createItemWithPictureUrl],
+        notAuctionedOff: [data.createItemWithPictureUrl]
+      };
+
+      if (
+        cachedData.all &&
+        cachedData.all.itemsOwned &&
+        cachedData.all.itemsOwned.count
+      ) {
         // if there are cached items
-        count = cachedData.itemsOwned.count + 1;
-        items = [...cachedData.itemsOwned.items, data.createItemWithPictureUrl];
+        count.all = cachedData.all.itemsOwned.count + 1;
+        items.all = [
+          ...cachedData.all.itemsOwned.items,
+          data.createItemWithPictureUrl
+        ];
+      }
+      if (
+        cachedData.notAuctionedOff &&
+        cachedData.notAuctionedOff.itemsOwned &&
+        cachedData.notAuctionedOff.itemsOwned.count
+      ) {
+        count.notAuctionedOff = cachedData.notAuctionedOff.itemsOwned.count + 1;
+        items.notAuctionedOff = [
+          ...cachedData.notAuctionedOff.itemsOwned.items,
+          data.createItemWithPictureUrl
+        ];
       }
 
-      store.writeQuery<ItemsOwnedQuery>({
+      store.writeQuery<ItemsOwnedQuery, ItemsOwnedQueryVariables>({
         query: ItemsOwnedDocument,
         data: {
-          itemsOwned: { count, items, __typename: "UserItemsResponse" }
+          itemsOwned: {
+            count: count.all,
+            items: items.all,
+            __typename: "UserItemsResponse"
+          }
+        },
+        variables: {
+          excludeAuctionedOff: false
+        }
+      });
+
+      store.writeQuery<ItemsOwnedQuery, ItemsOwnedQueryVariables>({
+        query: ItemsOwnedDocument,
+        data: {
+          itemsOwned: {
+            count: count.notAuctionedOff,
+            items: items.notAuctionedOff,
+            __typename: "UserItemsResponse"
+          }
+        },
+        variables: {
+          excludeAuctionedOff: true
         }
       });
     }

ac65cd5f609afbc30cc8cd3c944edf71e383e4d5
Tim <harvard90873@gmail.com>
2021-12-26 13:41:22 +0100

    web: add initial auction details page

---
+++ packages/web/src/pages/auction/[id].tsx
@@ -0,0 +1,33 @@
+import { useGetAuctionQuery } from "client-controllers";
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import { withApollo } from "../../utils/withApollo";
+
+const AuctionInfo: NextPage = () => {
+  const { query } = useRouter();
+  const id = query.id as string;
+  const { data } = useGetAuctionQuery({
+    variables: { auctionId: id }
+  });
+
+  return (
+    <div className="flex justify-center items-center w-full h-full p-6">
+      {!data ? (
+        <>loading</>
+      ) : !data.getAuction ? (
+        <>No such auction.</>
+      ) : (
+        <div className="border rounded-lg w-full p-8 max-w-4xl border-gray-500 bg-neutral-800">
+          <h2>{data.getAuction.title}</h2>
+          <hr className="mb-5" />
+          <img
+            src={data.getAuction.item.picture}
+            className="w-full h-auto max-w-md rounded-lg"
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default withApollo({ ssr: true })(AuctionInfo);

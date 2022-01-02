a853ed2057f6e40ee7cb271d8b4069330ef88a75
Tim <harvard90873@gmail.com>
2022-01-02 12:01:21 +0100

    web: handle transparent bg images

--- packages/web/src/components/ItemComponent.tsx
+++ packages/web/src/components/ItemComponent.tsx
@@ -15,19 +15,18 @@ export const Item: FC<ItemComponentProps> = ({ item }) => {
 
   return (
     <li
-      className="w-60 h-72 p-3 bg-neutral-900 border border-gray-500 border-opacity-60 cursor-pointer rounded-lg transition-all duration-300 hover:scale-[1.02] hover:bg-gray-800"
+      className="w-60 h-72 bg-neutral-900 border border-gray-500 border-opacity-60 cursor-pointer rounded-lg transition-all duration-300 hover:scale-[1.02] hover:bg-gray-800"
       key={item.id}
     >
       <VStack className="items-center gap-4">
-        <img
-          src={item.picture}
-          className="w-auto h-28 object-cover rounded-lg"
-        />
+        <div className="bg-slate-700 p-4 h-36 flex justify-center items-center">
+          <img src={item.picture} className="w-auto max-h-48 rounded-lg" />
+        </div>
         <h4>{item.name}</h4>
         <button
           className="danger-button"
-          onClick={() => {
-            deleteItem(
+          onClick={async () => {
+            await deleteItem(
               createItemDeletionOptions({
                 itemId: item.id
               })

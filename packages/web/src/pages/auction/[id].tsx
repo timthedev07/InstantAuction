56422ad5703771aabf813b0dbf66fb830c4ee07b
Tim <harvard90873@gmail.com>
2022-01-02 23:16:53 +0100

    web: handle non-existing auction

--- packages/web/src/pages/auction/[id].tsx
+++ packages/web/src/pages/auction/[id].tsx
@@ -1,6 +1,7 @@
 import { useGetAuctionQuery } from "client-controllers";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
+import { NotFoundPage } from "../../components/pages/404";
 import { withApollo } from "../../utils/withApollo";
 
 const AuctionInfo: NextPage = () => {
@@ -11,22 +12,24 @@ const AuctionInfo: NextPage = () => {
   });
 
   return (
-    <div className="flex justify-center items-center w-full h-full p-6">
+    <>
       {!data ? (
         <>loading</>
       ) : !data.getAuction ? (
-        <>No such auction.</>
+        <NotFoundPage />
       ) : (
-        <div className="border rounded-lg w-full p-8 max-w-4xl border-gray-500 bg-neutral-800">
-          <h2>{data.getAuction.title}</h2>
-          <hr className="mb-5" />
-          <img
-            src={data.getAuction.item.picture}
-            className="w-full h-auto max-w-md rounded-lg"
-          />
+        <div className="flex justify-center items-center w-full h-full p-6">
+          <div className="border rounded-lg w-full p-8 max-w-4xl border-gray-500 bg-neutral-800">
+            <h2>{data.getAuction.title}</h2>
+            <hr className="mb-5" />
+            <img
+              src={data.getAuction.item.picture}
+              className="w-full h-auto max-w-md rounded-lg"
+            />
+          </div>
         </div>
       )}
-    </div>
+    </>
   );
 };
 

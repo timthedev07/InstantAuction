67da83aaa2a7390dfc9c38fd96f6ecbfc9aabce8
Tim <harvard90873@gmail.com>
2022-01-02 22:57:36 +0100

    web: refactor create auction component

--- packages/web/src/components/CreateAuction.tsx
+++ packages/web/src/components/CreateAuction.tsx
@@ -1,10 +1,19 @@
 import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  Select,
+  Textarea
+} from "@chakra-ui/react";
+import {
   accessErrMessage,
   createAuctionCreationOptions,
   useCreateAuctionMutation,
   useItemsOwnedQuery
 } from "client-controllers";
-import { ErrorMessage, Field, Form, Formik } from "formik";
+import { useFormik } from "formik";
+import { useRouter } from "next/router";
 import { FC } from "react";
 import { useAlert } from "../contexts/AlertContext";
 import { clearValuesOnUndefined } from "../utils/processFormikValidatorReturn";
@@ -14,89 +23,113 @@ export const CreateAuction: FC = ({}) => {
   const { data } = useItemsOwnedQuery({
     variables: { excludeAuctionedOff: true }
   });
+  const router = useRouter();
   const { triggerAlert } = useAlert();
+  const formik = useFormik({
+    initialValues: { title: "", description: "", itemId: "-1" },
+    validate: ({ description, title, itemId }) => {
+      const returned = {
+        itemId:
+          !itemId || itemId === "-1"
+            ? "Please select an item to sell."
+            : undefined,
+        description: !description
+          ? "Please provide a description"
+          : description.length > 400
+          ? "Must be within 400 characters"
+          : undefined,
+        title: !title
+          ? "Please provide a title for the auction"
+          : title.length > 100
+          ? "Must be within 100 characters"
+          : undefined
+      };
+      return clearValuesOnUndefined(returned);
+    },
+    onSubmit: async ({ title, description, itemId }) => {
+      const itemIdNum = parseInt(itemId);
+
+      try {
+        const result = await createAuction(
+          createAuctionCreationOptions({
+            description,
+            title,
+            itemId: itemIdNum
+          })
+        );
+
+        if (!result || !result.data || !result.data.createAuction) {
+          triggerAlert("Something unexpected happened.");
+        }
+
+        triggerAlert("Auction created!", "success", () => {
+          router.push(`/auction/${result.data!.createAuction.id}`);
+        });
+      } catch (err) {
+        triggerAlert(accessErrMessage(err));
+      }
+    }
+  });
 
   return (
-    <div className="m-6 border-white border rounded p-5">
+    <div className="m-6 border rounded p-5">
       <h3>Create Auction</h3>
-      <Formik
-        initialValues={{ title: "", description: "", itemId: "" }}
-        validate={({ description, title }) => {
-          const returned = {
-            description: !description
-              ? "Required"
-              : description.length > 400
-              ? "Must be within 400 characters"
-              : undefined,
-            title: !title
-              ? "Required"
-              : title.length > 100
-              ? "Must be within 100 characters"
-              : undefined
-          };
-          return clearValuesOnUndefined(returned);
-        }}
-        onSubmit={async ({ title, description, itemId }) => {
-          const itemIdNum = parseInt(itemId);
 
-          if (itemIdNum === -1) {
-            return;
-          }
+      <form className="flex flex-col gap-3 m-3">
+        <FormControl isRequired isInvalid={!!formik.errors.title}>
+          <FormLabel as="legend" htmlFor="title-input">
+            Auction Title
+          </FormLabel>
+          <Input
+            onChange={formik.handleChange}
+            value={formik.values.title}
+            id="title-input"
+            name="title"
+          />
+          <FormErrorMessage>{formik.errors.title}</FormErrorMessage>
+        </FormControl>
 
-          try {
-            await createAuction(
-              createAuctionCreationOptions({
-                description,
-                title,
-                itemId: itemIdNum
-              })
-            );
-            triggerAlert("Auction created!", "success");
-          } catch (err) {
-            triggerAlert(accessErrMessage(err));
-          }
-        }}
-      >
-        {({ errors }) => (
-          <Form>
-            <Field
-              name="title"
-              className="px-4 py-3 rounded bg-gray-800 bg-opacity-90 border border-gray-50"
-            />
-            <ErrorMessage name="title" />
-            <br />
-            <Field
-              className="bg-primary-700 rounded py-2 px-4"
-              name="itemId"
-              as="select"
-              data-tip-disable={!errors.itemId}
-              data-tip={errors.itemId}
-            >
-              <option disabled value="">
-                -Select Item-
-              </option>
-              {data &&
-                data.itemsOwned.items.map(each => (
-                  <option key={each.name} value={each.id}>
-                    {each.name}
-                  </option>
-                ))}
-            </Field>
-            <ErrorMessage name="itemId" />
-            <br />
-            <Field
-              name="description"
-              as="textarea"
-              className="px-4 py-3 rounded bg-gray-800 bg-opacity-90 border border-gray-50"
-              data-tip-disable={!errors.description}
-              data-tip={errors.description}
-            />
-            <ErrorMessage name="description" />
-            <br />
-            <button type="submit">Create</button>
-          </Form>
-        )}
-      </Formik>
+        <FormControl isRequired isInvalid={!!formik.errors.itemId}>
+          <FormLabel as="legend" htmlFor={"item-id-select"}>
+            Item
+          </FormLabel>
+          <Select
+            value={formik.values.itemId}
+            onChange={formik.handleChange}
+            id="item-id-select"
+            name="itemId"
+          >
+            <option disabled value="-1">
+              -Select Item-
+            </option>
+            {data &&
+              data.itemsOwned.items.map(each => (
+                <option key={each.name} value={each.id}>
+                  {each.name}
+                </option>
+              ))}
+          </Select>
+          <FormErrorMessage>Please select an item to sell.</FormErrorMessage>
+        </FormControl>
+
+        <FormControl isRequired isInvalid={!!formik.errors.description}>
+          <FormLabel htmlFor="description-textarea" as="legend">
+            Auction Description
+          </FormLabel>
+          <Textarea
+            value={formik.values.description}
+            onChange={formik.handleChange}
+            name="description"
+          />
+          <FormErrorMessage name="description">
+            {formik.errors.description}
+          </FormErrorMessage>
+        </FormControl>
+
+        <button className="cyan-button max-w-[100px]" type="submit">
+          Create
+        </button>
+      </form>
     </div>
   );
 };

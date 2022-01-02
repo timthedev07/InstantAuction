efdba26e5b5656f76c187fd8cfdfb6f28ca5cf5d
Tim <harvard90873@gmail.com>
2022-01-02 13:58:42 +0100

    client-controllers: update function

--- packages/client-controllers/src/utils/isServerDown.ts
+++ packages/client-controllers/src/utils/isServerDown.ts
@@ -1,6 +1,10 @@
 export const isServerDown = (queryCallError: any) => {
-  return (
-    queryCallError &&
-    (!queryCallError.graphQLErrors || queryCallError.graphQLErrors.length < 1)
-  );
+  try {
+    return (
+      queryCallError &&
+      (!queryCallError.graphQLErrors || queryCallError.graphQLErrors.length < 1)
+    );
+  } catch (err) {
+    return false;
+  }
 };

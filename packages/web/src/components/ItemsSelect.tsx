820668e6a86a5a6822ca01d003ee2ecb9d440943
Tim <harvard90873@gmail.com>
2022-01-03 22:16:31 +0100

    web: create items select component

---
+++ packages/web/src/components/ItemsSelect.tsx
@@ -0,0 +1,56 @@
+import {
+  FormControl,
+  FormLabel,
+  Select,
+  FormErrorMessage
+} from "@chakra-ui/react";
+import { useItemsOwnedQuery } from "client-controllers";
+import { FC } from "react";
+
+interface ItemsSelectProps {
+  isInvalid?: boolean;
+  isRequired?: boolean;
+  value: string;
+  onChange: any;
+  onBlur: any;
+  name: string;
+}
+
+export const ItemsSelect: FC<ItemsSelectProps> = ({
+  isInvalid = false,
+  isRequired = true,
+  value,
+  onChange,
+  name,
+  onBlur
+}) => {
+  const { data } = useItemsOwnedQuery({
+    variables: { excludeAuctionedOff: true }
+  });
+
+  return (
+    <FormControl isRequired={isRequired} isInvalid={isInvalid}>
+      <FormLabel as="legend" htmlFor={"item-id-select"}>
+        Item
+      </FormLabel>
+      <Select
+        value={value}
+        onChange={onChange}
+        id="item-id-select"
+        name={name}
+        onBlur={onBlur}
+      >
+        <option disabled value="-1">
+          -Select Item-
+        </option>
+        {data &&
+          data.itemsOwned.items.map(each => (
+            <option key={each.name} value={each.id}>
+              {each.name}
+            </option>
+          ))}
+      </Select>
+      <FormErrorMessage>Please select an item to sell.</FormErrorMessage>
+    </FormControl>
+  );
+};

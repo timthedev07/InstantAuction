8a40a99096488bf1960d1b5d8febb97e42915ced
Tim <harvard90873@gmail.com>
2022-01-03 14:52:46 +0100

    server: new resolver

---
+++ packages/server/src/resolvers/bids/getBid.ts
@@ -0,0 +1,28 @@
+import { Resolver, Query, UseMiddleware, Ctx, Arg } from "type-graphql";
+import { bidExposedRelations } from "../../constants/exposed-relations";
+import { Auction } from "../../entity/Auction";
+import { Bid } from "../../entity/Bid";
+import { NetworkingContext } from "../../types/NetworkingContext";
+import { isAuthStrict } from "../../utils/isAuthMiddleware";
+
+@Resolver()
+export class GetBidResolver {
+  @Query(() => Bid, { nullable: true })
+  @UseMiddleware(isAuthStrict)
+  async getBid(
+    @Ctx() { req }: NetworkingContext,
+    @Arg("auctionId") auctionId: number
+  ) {
+    const auction = await Auction.findOne(auctionId, {
+      relations: ["bids", ...bidExposedRelations.map(each => "bids." + each)],
+    });
+
+    let resBid: Bid = undefined;
+    for (const bid of auction.bids) {
+      if (bid.bidder.id === req.session.userId) {
+        resBid = bid;
+      }
+    }
+    return resBid;
+  }
+}

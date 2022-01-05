e26aa14335c747aca28a3632b04951478bd5e1fb
Tim <harvard90873@gmail.com>
2022-01-05 20:02:12 +0100

    server: update bid exposed relations

--- packages/server/src/constants/exposed-relations.ts
+++ packages/server/src/constants/exposed-relations.ts
@@ -1,2 +1,2 @@
 export const auctionExposedRelations = ["seller", "item", "winner"];
-export const bidExposedRelations = ["bidder", "item", "item.owner"];
+export const bidExposedRelations = ["bidder", "item", "item.owner", "auction"];

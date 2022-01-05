14059dccd624e211569201176264f43b8489b860
Tim <harvard90873@gmail.com>
2022-01-05 18:01:59 +0100

    server: return count from query

--- packages/server/src/resolvers/bids/getUserBids.ts
+++ packages/server/src/resolvers/bids/getUserBids.ts
@@ -1,19 +1,40 @@
-import { Resolver, Query, UseMiddleware, Ctx } from "type-graphql";
+import {
+  Resolver,
+  Query,
+  UseMiddleware,
+  Ctx,
+  ObjectType,
+  Field,
+  Int,
+} from "type-graphql";
 import { bidExposedRelations } from "../../constants/exposed-relations";
 import { Bid } from "../../entity/Bid";
 import { NetworkingContext } from "../../types/NetworkingContext";
 import { isAuthStrict } from "../../utils/isAuthMiddleware";
 
+@ObjectType()
+export class BidsResponse {
+  @Field(() => Int)
+  count: number;
+
+  @Field(() => [Bid])
+  bids: Bid[];
+}
+
 @Resolver()
 export class GetUserBidsResolver {
-  @Query(() => [Bid])
+  @Query(() => BidsResponse)
   @UseMiddleware(isAuthStrict)
-  getUserBids(@Ctx() { req }: NetworkingContext) {
-    return Bid.find({
+  async getUserBids(@Ctx() { req }: NetworkingContext): Promise<BidsResponse> {
+    const [bids, count] = await Bid.findAndCount({
       where: {
         "bidder.id": req.session.userId,
       },
       relations: bidExposedRelations,
     });
+    return {
+      bids,
+      count,
+    };
   }
 }

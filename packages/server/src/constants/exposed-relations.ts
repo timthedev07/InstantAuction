01877691b006e4a2e477968f520700caecc4d3aa
Tim <harvard90873@gmail.com>
2022-01-06 11:40:51 +0100

    server: expand bix exposed relations

--- packages/server/src/constants/exposed-relations.ts
+++ packages/server/src/constants/exposed-relations.ts
@@ -1,2 +1,8 @@
 export const auctionExposedRelations = ["seller", "item", "winner"];
-export const bidExposedRelations = ["bidder", "item", "item.owner", "auction"];
+export const bidExposedRelations = [
+  "bidder",
+  "item",
+  "item.owner",
+  "auction",
+  "auction.item",
+];

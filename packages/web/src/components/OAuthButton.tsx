4bab4ac93e6fc9b6749a72b8c9f2c6e4039e2b64
Tim <harvard90873@gmail.com>
2022-01-06 11:13:56 +0100

    web: migrating from svgs to components

--- packages/web/src/components/OAuthButton.tsx
+++ packages/web/src/components/OAuthButton.tsx
@@ -1,29 +1,37 @@
 import Link from "next/link";
 import React from "react";
 import { capitalize } from "client-controllers";
+import { DiscordIcon } from "../icons/oauth/DiscordIcon";
+import { GoogleIcon } from "../icons/oauth/GoogleIcon";
+import { MicrosoftIcon } from "../icons/oauth/MicrosoftIcon";
+
+type ProviderType = "google" | "discord" | "microsoft";
 
 interface OAuthButtonProps {
-  provider: "google" | "discord" | "microsoft";
+  provider: ProviderType;
   href: string | null | undefined;
   style?: React.CSSProperties;
 }
+
+const ICON_MAP = {
+  discord: DiscordIcon,
+  google: GoogleIcon,
+  microsoft: MicrosoftIcon
+};
 
 export const OAuthButton: React.FC<OAuthButtonProps> = ({
   provider,
   href,
   style
 }) => {
+  const Icon = ICON_MAP[provider];
   return !href ? (
     <button>No href provided</button>
   ) : (
     <Link href={href} passHref>
       <button className={`oauth-button oauth-button-${provider}`} style={style}>
         {capitalize(provider)}
-        <img
-          className="oauth-button__icon"
-          src={`/images/icons/oauth/${provider}.svg`}
-          alt=""
-        />
+        <Icon className="oauth-button__icon" />
       </button>
     </Link>
   );

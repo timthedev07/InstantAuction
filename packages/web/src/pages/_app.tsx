cb3395d23f0e4d132902edd2a9e80ad7b887779e
Tim <harvard90873@gmail.com>
2022-01-07 16:52:40 +0100

    web: remove redundant prop

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -3,7 +3,6 @@ import type { AppProps } from "next/app";
 import { Layout } from "../components/layout";
 import { IconContext } from "@react-icons/all-files/lib";
 import { ChakraProvider } from "@chakra-ui/react";
-import { chakraTheme } from "../utils/chakraTheme";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (

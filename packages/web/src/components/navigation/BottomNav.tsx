42642ec3ee8001f082586100d792745227daada0
Tim <harvard90873@gmail.com>
2022-01-07 14:47:35 +0100

    web: sticky bottom nav

--- packages/web/src/components/navigation/BottomNav.tsx
+++ packages/web/src/components/navigation/BottomNav.tsx
@@ -6,7 +6,7 @@ interface BottomNavProps {}
 
 export const BottomNav: FC<BottomNavProps> = ({}) => {
   return (
-    <nav className="w-full flex justify-around items-center fixed bottom-0 bg-neutral-800 text-white">
+    <nav className="w-full flex justify-around items-center sticky bottom-0 bg-neutral-800 text-white">
       <ul className="flex items-center w-full justify-center gap-5 p-2 float-right">
         {LINKS.map(each => (
           <Link href={each.route} key={each.name}>

51350205903a678277b6707651d9150e396d11b1
Tim <harvard90873@gmail.com>
2022-01-08 19:20:00 +0100

    web: change login page colors

--- packages/web/src/components/pages/LoginPage.tsx
+++ packages/web/src/components/pages/LoginPage.tsx
@@ -40,9 +40,9 @@ export const LoginPage: FC<LoginPageProps> = ({}) => {
         title: "Login"
       })}
       <div className="flex justify-center items-center h-70vh min-h-450">
-        <div className="flex bg-neutral-800 rounded-lg w-9/12 h-9/10">
+        <div className="flex bg-neutral-100 rounded-lg w-9/12 h-9/10 shadow-2xl">
           <div className="flex flex-col justify-start items-center bg-transparent w-full md:w-5/12 lg:w-2/4 h-full pt-7">
-            <h3 className="border-b border-neutral-700 w-10/12 text-center">
+            <h3 className="border-b border-neutral-400 w-10/12 text-center">
               Sign in
             </h3>
             <div className="flex flex-col h-2/4 justify-center items-center w-full px-4">

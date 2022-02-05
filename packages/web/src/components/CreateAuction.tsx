5e75f92843918f416756a20cd6706dcc784d1c2c
Tim <harvard90873@gmail.com>
2022-02-05 18:51:04 +0100

    web: use component in auction creation form

--- packages/web/src/components/CreateAuction.tsx
+++ packages/web/src/components/CreateAuction.tsx
@@ -16,6 +16,7 @@ import { FC } from "react";
 import { useAlert } from "../contexts/AlertContext";
 import { clearValuesOnUndefined } from "../utils/processFormikValidatorReturn";
 import { ItemsSelect } from "./ItemsSelect";
+import { ItemUploadRedirectButton } from "./ItemUploadRedirectButton";
 
 export const CreateAuction: FC = ({}) => {
   const [createAuction] = useCreateAuctionMutation();
@@ -94,6 +95,7 @@ export const CreateAuction: FC = ({}) => {
           onBlur={formik.handleBlur}
           onChange={formik.handleChange}
         />
+        <ItemUploadRedirectButton />
 
         <FormControl isRequired isInvalid={!!formik.errors.description}>
           <FormLabel htmlFor="description-textarea" as="legend">

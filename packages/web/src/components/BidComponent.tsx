ed1f1ab136f30fa3cfc6f6c5cfd24afcd30acca4
Tim <harvard90873@gmail.com>
2022-02-05 14:18:54 +0100

    web: fix big list item bg color toggle

--- packages/web/src/components/BidComponent.tsx
+++ packages/web/src/components/BidComponent.tsx
@@ -39,10 +39,10 @@ export const BidListItem: FC<BidListItemProps> = ({
     <div
       key={bid.id}
       {...rest}
-      className={`bg-slate-100 border min-w-[250px] cursor-pointer transition duration-500 w-40 rounded p-4 ${
+      className={`border min-w-[250px] cursor-pointer transition duration-500 w-40 rounded p-4 ${
         isChosen && isSelectable
           ? "transform -translate-y-1 shadow-xl bg-slate-300"
-          : ""
+          : "bg-slate-100"
       }`}
     >
       <h3>

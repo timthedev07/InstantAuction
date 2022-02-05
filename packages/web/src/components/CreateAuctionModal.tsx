6f4fd2c2084e530b56875edb666c9d720dd5a1a0
Tim <harvard90873@gmail.com>
2022-02-05 18:35:59 +0100

    web: create auction creation modal

---
+++ packages/web/src/components/CreateAuctionModal.tsx
@@ -0,0 +1,64 @@
+import { FC } from "react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  Divider
+} from "@chakra-ui/react";
+import { CreateAuction } from "./CreateAuction";
+
+interface CreateAuctionModalProps {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+  className?: string;
+}
+
+export const CreateAuctionModal: FC<CreateAuctionModalProps> = ({
+  isOpen,
+  onClose,
+  onOpen,
+  className = ""
+}) => {
+  return (
+    <>
+      <Button
+        className={className}
+        variant={"solid"}
+        colorScheme="linkedin"
+        onClick={onOpen}
+      >
+        Create Auction
+      </Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent
+          maxW="3xl"
+          className="max-w-full md:max-w-[80%] lg:max-w-[60%]"
+        >
+          <ModalHeader>Create Auction</ModalHeader>
+
+          <ModalCloseButton />
+
+          <ModalBody>
+            <CreateAuction />
+          </ModalBody>
+
+          <Divider />
+
+          <ModalFooter>
+            <button className="orange-button" onClick={onClose}>
+              Cancel
+            </button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};

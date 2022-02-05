82296c188637dd360502433a37dd5ed3e5359f5e
Tim <harvard90873@gmail.com>
2022-02-05 10:59:16 +0100

    web: clear selection button

--- packages/web/src/components/EndAuctionModal.tsx
+++ packages/web/src/components/EndAuctionModal.tsx
@@ -12,7 +12,8 @@ import {
 } from "@chakra-ui/react";
 import { GetAuctionQuery } from "client-controllers";
 import { InfoIcon } from "../icons/InfoIcon";
-import { HStack } from "./utils/Stack";
+import { HStack, VStack } from "./utils/Stack";
+import { MdCancel } from "@react-icons/all-files/md/MdCancel";
 
 interface EndAuctionModal {
   isOpen: boolean;
@@ -60,30 +61,47 @@ export const EndAuctionModal: FC<EndAuctionModal> = ({
 
           <ModalCloseButton />
 
-          <ModalBody className="my-4">
-            <HStack>
-              {auction &&
-                auction.bids.map(each => {
-                  return (
-                    <div
-                      key={each.id}
-                      onClick={() => {
-                        setChosenBidId(each.id);
-                      }}
-                      className={`cursor-pointer transition duration-500 w-48 rounded p-4 ${
-                        each.id === chosenBidId
-                          ? "transform scale-105 shadow-xl bg-primary-300"
-                          : ""
-                      }`}
-                    >
-                      <h3>
-                        {each.item.name} from {each.bidder.username}
-                      </h3>
-                      <img src={each.item.picture} />
-                    </div>
-                  );
-                })}
-            </HStack>
+          <ModalBody data-bid-deselect="true" className="my-4">
+            <VStack className="gap-6">
+              <div className="w-48">
+                <Button
+                  leftIcon={<MdCancel />}
+                  colorScheme="orange"
+                  variant="solid"
+                  size="md"
+                  w="full"
+                  onClick={event => {
+                    // un select handler
+                    setChosenBidId(-1);
+                  }}
+                >
+                  Clear Selection
+                </Button>
+              </div>
+              <HStack className="w-full">
+                {auction &&
+                  auction.bids.map(each => {
+                    return (
+                      <div
+                        key={each.id}
+                        onClick={() => {
+                          setChosenBidId(each.id);
+                        }}
+                        className={`border min-w-[250px] cursor-pointer transition duration-500 w-40 rounded p-4 ${
+                          each.id === chosenBidId
+                            ? "transform scale-105 shadow-xl bg-primary-300"
+                            : ""
+                        }`}
+                      >
+                        <h3>
+                          {each.item.name} from {each.bidder.username}
+                        </h3>
+                        <img src={each.item.picture} />
+                      </div>
+                    );
+                  })}
+              </HStack>
+            </VStack>
           </ModalBody>
 
           <ModalFooter>

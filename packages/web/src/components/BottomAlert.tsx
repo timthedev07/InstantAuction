b82fd0c57ee607686f91ee9fa09033dfb1f16209
Tim <harvard90873@gmail.com>
2022-02-10 21:02:33 +0100

    web: update bottom alert text color

--- packages/web/src/components/BottomAlert.tsx
+++ packages/web/src/components/BottomAlert.tsx
@@ -7,7 +7,7 @@ interface CannotReachServerProps {
 export const CannotReachServer: FC<CannotReachServerProps> = ({ open }) => {
   return (
     <div
-      className={`right-7 fixed flex justify-center items-center p-3 w-48 h-11 rounded-md bg-red-800 transition duration-300 ${
+      className={`right-7 text-white fixed flex justify-center items-center p-3 w-48 h-11 rounded-md bg-red-800 transition duration-300 ${
         open ? "bottom-7" : "-bottom-[100vw]"
       }`}
     >
